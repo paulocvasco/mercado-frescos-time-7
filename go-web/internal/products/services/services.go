0744096d207a15363f3b2803a230bf143f1dba18
rodneyems <rodney.martins@live.com>
2022-06-03 18:50:35 -0300

    feature: implement service layer

--- go-web/internal/products/services/services.go
+++ go-web/internal/products/services/services.go
@@ -1,6 +1,7 @@
-package services
+package service
 
 import (
+	"errors"
 	"mercado-frescos-time-7/go-web/internal/products/model"
 	r "mercado-frescos-time-7/go-web/internal/products/repository"
 )
@@ -9,7 +10,7 @@ type Service interface {
 	Insert(product model.Product) (model.Product, error)
 	GetAll() ([]model.Product, error)
 	GetById(id int) (model.Product, error)
-	Update(id int, product model.Product) (model.Product, error)
+	Update(id int, product model.Product) error
 	Delete(id int) error
 }
 
@@ -24,21 +25,51 @@ func NewService(r r.Repository) Service {
 }
 
 func (s *service) Insert(product model.Product) (model.Product, error) {
-
+	id, err := s.repository.LastId()
+	if err != nil {
+		return model.Product{}, errors.New("erro interno tente mais tarde")
+	}
+	product.Id = id
+	p, err := s.repository.Insert(product)
+	if err != nil {
+		return model.Product{}, errors.New("erro interno tente mais tarde")
+	}
+	return p, nil
 }
 
 func (s *service) GetAll() ([]model.Product, error) {
-
+	pp, err := s.repository.GetAll()
+	if err != nil {
+		return nil, err
+	}
+	return pp, nil
 }
 
 func (s *service) GetById(id int) (model.Product, error) {
-
+	p, err := s.repository.GetById(id)
+	if err != nil {
+		return model.Product{}, err
+	}
+	return p, err
 }
 
-func (s *service) Update(id int, product model.Product) (model.Product, error) {
-
+func (s *service) Update(id int, product model.Product) error {
+	oldProduct, err := s.repository.GetById(id)
+	if err != nil {
+		return err
+	}
+	product.Id = oldProduct.Id
+	err = s.repository.Update(product)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (s *service) Delete(id int) error {
-
+	err := s.repository.Delete(id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
