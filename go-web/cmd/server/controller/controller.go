3de1c56562735ae637c9de53e1aed1358199b40a
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-06-03 15:05:00 -0300

    Implement all methods on controller

--- go-web/cmd/server/controller/controller.go
+++ go-web/cmd/server/controller/controller.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"io/ioutil"
+	customerrors "mercado-frescos-time-7/go-web/internal/custom_errors"
 	"mercado-frescos-time-7/go-web/internal/warehouse/services"
 	"net/http"
 
@@ -8,11 +10,11 @@ import (
 )
 
 type Controller interface {
-	GetToken(*gin.Context)
 	GetAll(*gin.Context)
 	GetByID(*gin.Context)
-	Store(*gin.Context)
+	Create(*gin.Context)
 	Update(*gin.Context)
+	Delete(*gin.Context)
 }
 
 type controller struct {
@@ -28,82 +30,85 @@ func (control *controller) GetAll(c *gin.Context) {
 	c.JSON(http.StatusOK, response)
 }
 
-// func (control *controller) GetByID(c *gin.Context) {
-//	param := c.Param("id")
-//	response, err := control.service.GetByID(param)
-//	if err != nil {
-//		switch err {
-//		case customErrors.ErrorInvalidIDParameter:
-//			c.JSON(http.StatusBadRequest, err)
-//		case customErrors.ErrorInvalidID:
-//			c.JSON(http.StatusNotFound, err)
-//		default:
-//			c.JSON(http.StatusInternalServerError, err)
-//		}
-//		return
-//	}
-//	c.JSON(http.StatusOK, response)
-// }
+func (control *controller) GetByID(c *gin.Context) {
+	param := c.Param("id")
+	response, err := control.service.GetByID(param)
+	if err != nil {
+		switch err {
+		case customerrors.ErrorInvalidIDParameter:
+			c.JSON(http.StatusBadRequest, err)
+		case customerrors.ErrorInvalidID:
+			c.JSON(http.StatusNotFound, err)
+		default:
+			c.JSON(http.StatusInternalServerError, err)
+		}
+		return
+	}
+	c.JSON(http.StatusOK, response)
+}
 
-// func (control *controller) Store(c *gin.Context) {
-//	body := c.Request.Body
-//	defer body.Close()
+func (control *controller) Create(c *gin.Context) {
+	body := c.Request.Body
+	defer body.Close()
 
-//	data, err := ioutil.ReadAll(body)
-//	if err != nil {
-//		c.JSON(http.StatusBadRequest, err)
-//		return
-//	}
+	data, err := ioutil.ReadAll(body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
 
-//	err = control.service.Store(data)
-//	if err != nil {
-//		switch err {
-//		case customErrors.ErrorMissingCode:
-//			c.JSON(http.StatusBadRequest, err)
-//		case customErrors.ErrorMissingCoin:
-//			c.JSON(http.StatusBadRequest, err)
-//		case customErrors.ErrorMissingIssuer:
-//			c.JSON(http.StatusBadRequest, err)
-//		case customErrors.ErrorMissingReciever:
-//			c.JSON(http.StatusBadRequest, err)
-//		case customErrors.ErrorMissingValue:
-//			c.JSON(http.StatusBadRequest, err)
-//		default:
-//			c.JSON(http.StatusInternalServerError, err)
-//		}
-//		return
-//	}
-//	c.JSON(http.StatusOK, nil)
-// }
+	err = control.service.Create(data)
+	if err != nil {
+		switch err {
+		case customerrors.ErrorMissingAddres:
+			c.JSON(http.StatusUnprocessableEntity, err)
+		case customerrors.ErrorMissingTelephone:
+			c.JSON(http.StatusUnprocessableEntity, err)
+		case customerrors.ErrorMissingCapacity:
+			c.JSON(http.StatusUnprocessableEntity, err)
+		case customerrors.ErrorMissingTemperature:
+			c.JSON(http.StatusUnprocessableEntity, err)
+		default:
+			c.JSON(http.StatusInternalServerError, err)
+		}
+		return
+	}
+	c.JSON(http.StatusOK, nil)
+}
 
-// func (control *controller) Update(c *gin.Context) {
-//	id := c.Param("id")
-//	body := c.Request.Body
-//	defer body.Close()
+func (control *controller) Update(c *gin.Context) {
+	id := c.Param("id")
+	body := c.Request.Body
+	defer body.Close()
 
-//	data, err := ioutil.ReadAll(body)
-//	if err != nil {
-//		c.JSON(http.StatusBadRequest, err)
-//		return
-//	}
+	data, err := ioutil.ReadAll(body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
 
-//	err = control.service.Update(id, data)
-//	if err != nil {
-//		switch err {
-//		case customErrors.ErrorMissingCode:
-//			c.JSON(http.StatusBadRequest, err)
-//		case customErrors.ErrorMissingCoin:
-//			c.JSON(http.StatusBadRequest, err)
-//		case customErrors.ErrorMissingIssuer:
-//			c.JSON(http.StatusBadRequest, err)
-//		case customErrors.ErrorMissingReciever:
-//			c.JSON(http.StatusBadRequest, err)
-//		case customErrors.ErrorMissingValue:
-//			c.JSON(http.StatusBadRequest, err)
-//		default:
-//			c.JSON(http.StatusInternalServerError, err)
-//		}
-//		return
-//	}
-//	c.JSON(http.StatusOK, nil)
-// }
+	err = control.service.Update(id, data)
+	if err != nil {
+		switch err {
+		case customerrors.ErrorInvalidIDParameter:
+			c.JSON(http.StatusNotFound, err)
+		case customerrors.ErrorInvalidID:
+			c.JSON(http.StatusNotFound, err)
+		default:
+			c.JSON(http.StatusInternalServerError, err)
+		}
+		return
+	}
+	c.JSON(http.StatusOK, nil)
+}
+
+func (control *controller) Delete(c *gin.Context) {
+	id := c.Param("id")
+
+	err := control.service.Delete(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, err)
+		return
+	}
+	c.JSON(http.StatusNoContent, err)
+}
