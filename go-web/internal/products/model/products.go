6f68174dd39877c0c079b6a7e23089bd7a5d3984
rodneyems <rodney.martins@live.com>
2022-06-03 14:11:49 -0300

    refactor: update product fields type

--- go-web/internal/products/model/products.go
+++ go-web/internal/products/model/products.go
@@ -1,16 +1,16 @@
 package model
 
-type products struct {
-	Id                               int    `json:"id"`
-	Product_code                     string `json:"product_code"`
-	Description                      string `json:"description"`
-	Width                            int    `json:"width"`
-	Height                           int    `json:"height"`
-	Lenght                           int    `json:"lenght"`
-	Net_weight                       int    `json:"netweight"`
-	Expiration_rate                  int    `json:"expiration_rate"`
-	Recommended_freezing_temperature int    `json:"recommended_freezing_temperature"`
-	Freezing_rate                    string `json:"freezing_rate"`
-	Product_type_id                  string `json:"product_type_id" `
-	Seller_id                        string `json:"seller_id"`
+type Product struct {
+	Id                               int     `json:"id"`
+	Product_code                     string  `json:"product_code"`
+	Description                      string  `json:"description"`
+	Width                            int     `json:"width"`
+	Height                           float64 `json:"height"`
+	Lenght                           float64 `json:"lenght"`
+	Net_weight                       float64 `json:"netweight"`
+	Expiration_rate                  int     `json:"expiration_rate"`
+	Recommended_freezing_temperature int     `json:"recommended_freezing_temperature"`
+	Freezing_rate                    float64 `json:"freezing_rate"`
+	Product_type_id                  int     `json:"product_type_id" `
+	Seller_id                        int     `json:"seller_id"`
 }
