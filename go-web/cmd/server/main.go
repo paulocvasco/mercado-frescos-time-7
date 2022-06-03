2397a8e6a575c1246bd0922404843574a63d94a1
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-06-03 17:56:01 -0300

    Create package routers and instance services

--- go-web/cmd/server/main.go
+++ go-web/cmd/server/main.go
@@ -1,27 +1,15 @@
 package main
 
 import (
-	"mercado-frescos-time-7/go-web/cmd/server/controller"
-	"mercado-frescos-time-7/go-web/internal/warehouse"
+	"mercado-frescos-time-7/go-web/cmd/server/routers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	repository := warehouse.NewRepository()
-	service := warehouse.NewService(repository)
-	controller := controller.NewController(service)
+	r := gin.Default()
 
-	routers := gin.Default()
-	mapRouters(routers, controller)
-	routers.Run()
-}
+	routers.InstanceWarehouse(r)
 
-func mapRouters(r *gin.Engine, c controller.Controller) {
-	wr := r.Group("/api/v1/warehouses")
-	wr.GET("", c.GetAll)
-	wr.GET("/:id", c.GetByID)
-	wr.POST("", c.Create)
-	wr.PATCH("/:id", c.Update)
-	wr.DELETE("/:id", c.Delete)
+	r.Run()
 }
