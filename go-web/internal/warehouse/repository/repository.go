cebc2948d9b7e34a198341c85cba4dd6ce982a4d
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-06-03 13:31:21 -0300

    Rename file and create methods to Add and Update objects

---
+++ go-web/internal/warehouse/repository/repository.go
@@ -0,0 +1,62 @@
+package repository
+
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	customerrors "mercado-frescos-time-7/go-web/internal/custom_errors"
+	"strconv"
+	"strings"
+)
+
+type Warehouses struct {
+	Warehouse []Warehouse `json:"data"`
+}
+
+type Warehouse struct {
+	ID                 int    `json:"id"`
+	Address            string `json:"address"`
+	Telephone          string `json:"telephone"`
+	WarehouseCode      string `json:"warehouse_code"`
+	MinimunCapacity    int    `json:"minimun_capacity"`
+	MinimunTemperature int    `json:"minimun_temperature"`
+}
+
+func (w *Warehouses) Create(new Warehouse) {
+	new.ID = len(w.Warehouse) + 1
+	new.WarehouseCode = calculateCode(new.Address, new.Telephone, strconv.Itoa(new.MinimunCapacity), strconv.Itoa(new.MinimunTemperature))
+
+	w.Warehouse = append(w.Warehouse, new)
+}
+
+func (w *Warehouses) Update(id int, newValues Warehouse) error {
+	if id < 0 || id > len(w.Warehouse) {
+		return customerrors.ErrorInvalidID
+	}
+
+	warehouse := w.Warehouse[id]
+	if newValues.Address != "" {
+		warehouse.Address = newValues.Address
+	}
+	if newValues.Telephone != "" {
+		warehouse.Telephone = newValues.Telephone
+	}
+	if newValues.MinimunCapacity != 0 {
+		warehouse.MinimunCapacity = newValues.MinimunCapacity
+	}
+	if newValues.MinimunTemperature != 0 {
+		warehouse.MinimunCapacity = newValues.MinimunTemperature
+	}
+
+	w.Warehouse[id] = newValues
+	return nil
+}
+
+func calculateCode(info ...string) string {
+	var builder strings.Builder
+	for _, s := range info {
+		builder.WriteString(s)
+	}
+	code := sha256.Sum256([]byte(builder.String()))
+
+	return base64.StdEncoding.EncodeToString(code[:])
+}
