48516a321990d031ed556e5ba502fea1d3442f54
rodneyems <rodney.martins@live.com>
2022-06-03 18:52:34 -0300

    feature: add routes

---
+++ go-web/cmd/server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"mercado-frescos-time-7/go-web/cmd/server/controller"
+	"mercado-frescos-time-7/go-web/internal/products/repository"
+	service "mercado-frescos-time-7/go-web/internal/products/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	repo := repository.NewRepository()
+	serv := service.NewService(repo)
+	pr := controller.NewRepository(serv)
+
+	group := r.Group("/products")
+	{
+		group.GET("/", pr.GetAllProducts())
+		group.GET("/:id", pr.GetProduct())
+		group.DELETE("/:id", pr.DeleteProducts())
+		group.POST("/", pr.SaveProducts())
+		group.PUT("/:id", pr.UpdateProducts())
+	}
+
+	r.Run()
+}
