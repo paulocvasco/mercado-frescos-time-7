c00f73774ebf4730777872d49d72dc598fb0dc72
rodneyems <rodney.martins@live.com>
2022-06-03 18:51:14 -0300

    feature: implement repository layer

--- go-web/internal/products/repository/repository.go
+++ go-web/internal/products/repository/repository.go
@@ -1,13 +1,17 @@
 package repository
 
-import "mercado-frescos-time-7/go-web/internal/products/model"
+import (
+	"errors"
+	"mercado-frescos-time-7/go-web/internal/products/model"
+)
 
 type Repository interface {
 	Insert(product model.Product) (model.Product, error)
 	GetAll() ([]model.Product, error)
 	GetById(id int) (model.Product, error)
-	Update(id int, product model.Product) (model.Product, error)
+	Update(product model.Product) error
 	Delete(id int) error
+	LastId() (int, error)
 }
 
 type repository struct{}
@@ -16,22 +20,55 @@ func NewRepository() Repository {
 	return &repository{}
 }
 
-func (s *repository) Insert(product model.Product) (model.Product, error) {
+func (r *repository) Insert(product model.Product) (model.Product, error) {
+	model.Products = append(model.Products, product)
+	return product, nil
+}
+
+func (r *repository) GetAll() ([]model.Product, error) {
+	return model.Products, nil
+}
+
+func (r *repository) GetById(id int) (model.Product, error) {
+	p := model.Products
+	for _, v := range p {
+		if v.Id == id {
+			return v, nil
+		}
+	}
+	return model.Product{}, errors.New("id não encontrado")
+}
+
+func (r *repository) Update(product model.Product) error {
+	p := model.Products
+	for i, v := range p {
+		if v.Id == product.Id {
+			p[i] = product
+			return nil
+		}
+	}
+	return errors.New("id não encontrado")
 
 }
 
-func (s *repository) GetAll() ([]model.Product, error) {
-
+func (r *repository) Delete(id int) error {
+	p := model.Products
+	for i, v := range p {
+		if v.Id == id {
+			model.Products = append(p[:i], p[i+1:]...)
+			return nil
+		}
+	}
+	return errors.New("id não encontrado")
 }
 
-func (s *repository) GetById(id int) (model.Product, error) {
-
-}
-
-func (s *repository) Update(id int, product model.Product) (model.Product, error) {
-
-}
-
-func (s *repository) Delete(id int) error {
-
+func (r *repository) LastId() (int, error) {
+	ts := model.Products
+	maxId := ts[0].Id
+	for i := 1; i <= len(ts)-1; i++ {
+		if ts[i].Id > maxId {
+			maxId = ts[i].Id
+		}
+	}
+	return maxId + 1, nil
 }
