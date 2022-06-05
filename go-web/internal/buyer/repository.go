8dea92647127cd3d7575bbb8e9cc8840dab802ca
jandrade13 <jefferson.jandrade@mercadolivre.com>
2022-06-05 19:30:12 -0300

    feat created metodo GetAll

--- go-web/internal/buyer/repository.go
+++ go-web/internal/buyer/repository.go
@@ -1,9 +1,23 @@
 package buyer
 
+import b "mercado-frescos-time-7/go-web/internal/models"
+
 type Repository interface {
-	GetAll() ([]Buyer, error)
-	GetId(id int) (Buyer, error)
-	Creat(id, card_number_id int, first_name, last_name string) (Buyer, error)
-	Updat(id, card_number_id int, first_name, last_name string) (Buyer, error)
+	GetAll() ([]b.Buyers, error)
+	GetId(id int) (b.Buyer, error)
+	Creat(id, card_number_id int, first_name, last_name string) (b.Buyer, error)
+	Update(id, card_number_id int, first_name, last_name string) (b.Buyer, error)
 	Delete(id int) error
+}
+
+type repository struct{}
+
+var db []b.Buyers = []b.Buyers{}
+
+func NewRepository() Repository {
+	return &repository{}
+}
+
+func (r *repository) GetAll() ([]b.Buyers, error) {
+	return db, nil
 }
