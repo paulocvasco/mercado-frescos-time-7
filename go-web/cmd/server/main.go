7654ecc6892a9e84a81aa1b13a87c6339eaca6dc
rodneyems <rodney.martins@live.com>
2022-06-06 15:37:47 -0300

    refactor: move routes to a new file

--- go-web/cmd/server/main.go
+++ go-web/cmd/server/main.go
@@ -1,26 +1,15 @@
 package main
 
 import (
-	"mercado-frescos-time-7/go-web/cmd/server/controller"
-	"mercado-frescos-time-7/go-web/internal/products"
+	"mercado-frescos-time-7/go-web/cmd/server/routes"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	repo := products.NewRepository()
-	serv := products.NewService(repo)
-	pr := controller.NewRepository(serv)
 
-	group := r.Group("/products")
-	{
-		group.GET("/", pr.GetAllProducts())
-		group.GET("/:id", pr.GetProduct())
-		group.DELETE("/:id", pr.DeleteProducts())
-		group.POST("/", pr.SaveProducts())
-		group.PATCH("/:id", pr.UpdateProducts())
-	}
+	routes.InstanceProducts(r)
 
 	r.Run()
 }
---
+++ go-web/cmd/server/routes/products.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"mercado-frescos-time-7/go-web/cmd/server/controller"
+	"mercado-frescos-time-7/go-web/internal/products"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InstanceProducts(r *gin.Engine) {
+	repo := products.NewRepository()
+	serv := products.NewService(repo)
+	pr := controller.NewRepository(serv)
+
+	group := r.Group("/products")
+	{
+		group.GET("/", pr.GetAllProducts())
+		group.GET("/:id", pr.GetProduct())
+		group.DELETE("/:id", pr.DeleteProducts())
+		group.POST("/", pr.SaveProducts())
+		group.PATCH("/:id", pr.UpdateProducts())
+	}
+}
