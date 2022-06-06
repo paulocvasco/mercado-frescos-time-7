5bc104c9aafc95d0dec48cc9785039bcc27c1345
jandrade13 <jefferson.jandrade@mercadolivre.com>
2022-06-06 18:23:47 -0300

    tentando merge

--- go-web/cmd/server/routes/warehouse.go
+++ go-web/cmd/server/routes/warehouse.go
@@ -1,22 +1,22 @@
 package routes
 
-import (
-	"mercado-frescos-time-7/go-web/cmd/server/controller"
-	"mercado-frescos-time-7/go-web/internal/warehouse"
+// import (
+// 	"mercado-frescos-time-7/go-web/cmd/server/controller"
+// 	"mercado-frescos-time-7/go-web/internal/warehouse"
 
-	"github.com/gin-gonic/gin"
-)
+// 	"github.com/gin-gonic/gin"
+// )
 
-func InstanceWarehouse(r *gin.Engine) {
-	repository := warehouse.NewRepository()
-	service := warehouse.NewService(repository)
-	controller := controller.NewControllerWarehouse(service)
+// func InstanceWarehouse(r *gin.Engine) {
+// 	repository := warehouse.NewRepository()
+// 	service := warehouse.NewService(repository)
+// 	//controller := controller.NewControllerWarehouse(service)
 
-	wr := r.Group("/api/v1/warehouses")
-	wr.GET("/", controller.GetAllWarehouse)
-	wr.GET("/:id", controller.GetByIDWarehouse)
-	wr.POST("/", controller.CreateWarehouse)
-	wr.PATCH("/:id", controller.UpdateWarehouse)
-	wr.DELETE("/:id", controller.DeleteWarehouse)
+// 	wr := r.Group("/api/v1/warehouses")
+// 	wr.GET("/", controller.GetAllWarehouse)
+// 	wr.GET("/:id", controller.GetByIDWarehouse)
+// 	wr.POST("/", controller.CreateWarehouse)
+// 	wr.PATCH("/:id", controller.UpdateWarehouse)
+// 	wr.DELETE("/:id", controller.DeleteWarehouse)
 
-}
+// }
