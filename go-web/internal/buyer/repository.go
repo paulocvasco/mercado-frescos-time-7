9237577d8c2d1ffb4019cdb64c977812985e538f
jandrade13 <jefferson.jandrade@mercadolivre.com>
2022-06-05 21:05:11 -0300

    feat created metodo Creat

--- go-web/internal/buyer/repository.go
+++ go-web/internal/buyer/repository.go
@@ -1,23 +1,57 @@
 package buyer
 
-import b "mercado-frescos-time-7/go-web/internal/models"
+import (
+	"fmt"
+	"log"
+	b "mercado-frescos-time-7/go-web/internal/models"
+)
 
 type Repository interface {
-	GetAll() ([]b.Buyers, error)
+	GetAll() ([]b.Buyer, error)
 	GetId(id int) (b.Buyer, error)
 	Creat(id, card_number_id int, first_name, last_name string) (b.Buyer, error)
-	Update(id, card_number_id int, first_name, last_name string) (b.Buyer, error)
-	Delete(id int) error
+	// Update(id, card_number_id int, first_name, last_name string) (b.Buyer, error)
+	// Delete(id int) error
 }
 
-type repository struct{}
+var db []b.Buyer = []b.Buyer{}
 
-var db []b.Buyers = []b.Buyers{}
+type repository struct{}
 
 func NewRepository() Repository {
 	return &repository{}
 }
 
-func (r *repository) GetAll() ([]b.Buyers, error) {
+func (r *repository) GetAll() ([]b.Buyer, error) {
+	log.Print(db)
 	return db, nil
+}
+
+func (r *repository) GetId(id int) (b.Buyer, error) {
+	log.Println("cheguei GetId", id)
+	var getById b.Buyer
+	get := false
+	for i := range db {
+		if db[i].ID == id {
+			getById = db[i]
+			get = true
+		}
+
+	}
+	if !get {
+		return getById, fmt.Errorf("product %d não encontrado", id)
+	}
+
+	return getById, nil
+
+}
+func (r *repository) Creat(id, card_number_id int, first_name, last_name string) (b.Buyer, error) {
+	log.Println(db)
+	newBuyer := b.Buyer{id, card_number_id, first_name, last_name}
+	db = append(db, newBuyer)
+
+	log.Println(db)
+
+	return newBuyer, nil
+
 }
