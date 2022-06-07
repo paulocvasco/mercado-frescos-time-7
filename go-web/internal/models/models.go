d7fc48f90dcd3cfa168818fa35f418f49cf331a7
jandrade13 <jefferson.jandrade@mercadolivre.com>
2022-06-07 10:42:31 -0300

    fix: add binding in Buyer

--- go-web/internal/models/models.go
+++ go-web/internal/models/models.go
@@ -4,10 +4,10 @@ type Buyers struct {
 	Buyer []Buyer `json:"buyers"`
 }
 type Buyer struct {
-	ID           int    `json:"id"`
-	CardNumberID int    `json:"card_number_id"`
-	FirstName    string `json:"first_name"`
-	LastName     string `json:"last_name"`
+	ID           int    `json:"id" binding:"required"`
+	CardNumberID int    `json:"card_number_id" binding:"required"`
+	FirstName    string `json:"first_name" binding:"required"`
+	LastName     string `json:"last_name" binding:"required"`
 }
 type Warehouses struct {
 	Warehouse []Warehouse `json:"warehouses"`
