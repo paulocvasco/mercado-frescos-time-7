467d1e635f181a397949ef289b551c41abe3c0c9
rodneyems <rodney.martins@live.com>
2022-06-07 19:42:48 -0300

    refactor: update model imports

--- go-web/internal/products/repository.go
+++ go-web/internal/products/repository.go
@@ -2,13 +2,14 @@ package products
 
 import (
 	"errors"
+	"mercado-frescos-time-7/go-web/internal/models"
 )
 
 type Repository interface {
-	Insert(product Product) (Product, error)
-	GetAll() ([]Product, error)
-	GetById(id int) (Product, error)
-	Update(product Product) error
+	Insert(product models.Product) (models.Product, error)
+	GetAll() ([]models.Product, error)
+	GetById(id int) (models.Product, error)
+	Update(product models.Product) error
 	Delete(id int) error
 	LastId() (int, error)
 }
@@ -19,27 +20,27 @@ func NewRepository() Repository {
 	return &repository{}
 }
 
-func (r *repository) Insert(product Product) (Product, error) {
-	Products = append(Products, product)
+func (r *repository) Insert(product models.Product) (models.Product, error) {
+	models.Products = append(models.Products, product)
 	return product, nil
 }
 
-func (r *repository) GetAll() ([]Product, error) {
-	return Products, nil
+func (r *repository) GetAll() ([]models.Product, error) {
+	return models.Products, nil
 }
 
-func (r *repository) GetById(id int) (Product, error) {
-	p := Products
+func (r *repository) GetById(id int) (models.Product, error) {
+	p := models.Products
 	for _, v := range p {
 		if v.Id == id {
 			return v, nil
 		}
 	}
-	return Product{}, errors.New("id não encontrado")
+	return models.Product{}, errors.New("id não encontrado")
 }
 
-func (r *repository) Update(product Product) error {
-	p := Products
+func (r *repository) Update(product models.Product) error {
+	p := models.Products
 	for i, v := range p {
 		if v.Id == product.Id {
 			p[i] = product
@@ -51,10 +52,10 @@ func (r *repository) Update(product Product) error {
 }
 
 func (r *repository) Delete(id int) error {
-	p := Products
+	p := models.Products
 	for i, v := range p {
 		if v.Id == id {
-			Products = append(p[:i], p[i+1:]...)
+			models.Products = append(p[:i], p[i+1:]...)
 			return nil
 		}
 	}
@@ -62,7 +63,7 @@ func (r *repository) Delete(id int) error {
 }
 
 func (r *repository) LastId() (int, error) {
-	ts := Products
+	ts := models.Products
 	maxId := ts[0].Id
 	for i := 1; i <= len(ts)-1; i++ {
 		if ts[i].Id > maxId {
--- go-web/internal/products/services.go
+++ go-web/internal/products/services.go
@@ -3,15 +3,16 @@ package products
 import (
 	"encoding/json"
 	"errors"
+	"mercado-frescos-time-7/go-web/internal/models"
 
 	jsonpatch "github.com/evanphx/json-patch/v5"
 )
 
 type Service interface {
-	Insert(product Product) (Product, error)
-	GetAll() ([]Product, error)
-	GetById(id int) (Product, error)
-	Update(id int, product []byte) (Product, error)
+	Insert(product models.Product) (models.Product, error)
+	GetAll() ([]models.Product, error)
+	GetById(id int) (models.Product, error)
+	Update(id int, product []byte) (models.Product, error)
 	Delete(id int) error
 }
 
@@ -25,20 +26,20 @@ func NewService(r Repository) Service {
 	}
 }
 
-func (s *service) Insert(product Product) (Product, error) {
+func (s *service) Insert(product models.Product) (models.Product, error) {
 	id, err := s.repository.LastId()
 	if err != nil {
-		return Product{}, errors.New("erro interno tente mais tarde")
+		return models.Product{}, errors.New("erro interno tente mais tarde")
 	}
 	product.Id = id
 	p, err := s.repository.Insert(product)
 	if err != nil {
-		return Product{}, errors.New("erro interno tente mais tarde")
+		return models.Product{}, errors.New("erro interno tente mais tarde")
 	}
 	return p, nil
 }
 
-func (s *service) GetAll() ([]Product, error) {
+func (s *service) GetAll() ([]models.Product, error) {
 	pp, err := s.repository.GetAll()
 	if err != nil {
 		return nil, err
@@ -46,30 +47,30 @@ func (s *service) GetAll() ([]Product, error) {
 	return pp, nil
 }
 
-func (s *service) GetById(id int) (Product, error) {
+func (s *service) GetById(id int) (models.Product, error) {
 	p, err := s.repository.GetById(id)
 	if err != nil {
-		return Product{}, err
+		return models.Product{}, err
 	}
 	return p, err
 }
 
-func (s *service) Update(id int, product []byte) (Product, error) {
+func (s *service) Update(id int, product []byte) (models.Product, error) {
 	oldProduct, err := s.repository.GetById(id)
 	if err != nil {
-		return Product{}, err
+		return models.Product{}, err
 	}
 	oldProductJSON, _ := json.Marshal(oldProduct)
 	patch, err := jsonpatch.MergePatch(oldProductJSON, product)
 	if err != nil {
-		return Product{}, err
+		return models.Product{}, err
 	}
-	var updateProduct Product
+	var updateProduct models.Product
 	json.Unmarshal(patch, &updateProduct)
 
 	err = s.repository.Update(updateProduct)
 	if err != nil {
-		return Product{}, err
+		return models.Product{}, err
 	}
 	return updateProduct, nil
 }
