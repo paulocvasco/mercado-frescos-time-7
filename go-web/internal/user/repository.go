e545f760506aa0d10e0bc8db9129b9520f46ef81
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-06-07 23:02:29 -0300

    Rename file and add method get

--- go-web/internal/user/repository.go
+++ go-web/internal/user/repository.go
@@ -12,3 +12,12 @@ var users []User
 func (u *Users) NewUser(newUser User) {
 	users = append(users, newUser)
 }
+
+func (u *Users) GetUser(username string) (User, error) {
+	for _, user := range users {
+		if user.username == username {
+			return user, nil
+		}
+	}
+	return User{}, nil
+}
