e7d6f44382c58a0d41ae9976881384dc5a518b7a
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-06-07 23:51:25 -0300

    Improve controller methods

--- go-web/cmd/server/controller/user.go
+++ go-web/cmd/server/controller/user.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"mercado-frescos-time-7/go-web/internal/user"
 	"net/http"
 
@@ -16,16 +18,58 @@ type userController struct {
 	service user.Service
 }
 
-func NewController(s user.Service) UserController {
+func NewUserController(s user.Service) UserController {
 	return &userController{
 		service: s,
 	}
 }
 
 func (u *userController) NewUser(c *gin.Context) {
+	body := c.Request.Body
+	defer body.Close()
+
+	data, err := ioutil.ReadAll(body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	var newUser user.User
+	err = json.Unmarshal(data, &newUser)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	err = u.service.NewUser(newUser)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+	}
+
 	c.JSON(http.StatusOK, nil)
 }
 
 func (u *userController) GetToken(c *gin.Context) {
-	c.JSON(http.StatusOK, nil)
+	body := c.Request.Body
+	defer body.Close()
+
+	data, err := ioutil.ReadAll(body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	var requestUser user.User
+	err = json.Unmarshal(data, &requestUser)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	token, err := u.service.GetToken(requestUser)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"user_token": token})
 }
