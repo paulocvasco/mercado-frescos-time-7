44560354469041541aacc82de5479fc6888b053e
jandrade13 <jefferson.jandrade@mercadolivre.com>
2022-06-08 18:10:03 -0300

    criado as funcoes no repository

---
+++ go-web/internal/login/repository.go
@@ -0,0 +1,50 @@
+package login
+
+import (
+	"errors"
+	"mercado-frescos-time-7/go-web/internal/models"
+)
+
+type Repository interface {
+	GetUser(user string, password string) (models.User, error)
+	CreateUser(user string, password string) (data models.User)
+	GenerateID() int
+}
+
+var db []models.User = []models.User{}
+
+type repository struct{}
+
+func NewRepository() Repository {
+	return &repository{}
+}
+
+var lastId []int
+
+func (r *repository) GenerateID() int {
+
+	if len(db) == 0 {
+		lastId = append(lastId, 1)
+		return 1
+	}
+
+	result := lastId[len(lastId)-1] + 1
+	lastId = append(lastId, result)
+
+	return result
+}
+
+func (r *repository) GetUser(user string, password string) (models.User, error) {
+	for i, value := range db {
+		if value.User == user && value.Password == password {
+			return db[i], nil
+		}
+	}
+	return models.User{}, errors.New("Id não existe")
+}
+func (r *repository) CreateUser(user string, password string) models.User {
+	id := r.GenerateID()
+	newUser := models.User{Id: id, User: user, Password: password}
+	db = append(db, newUser)
+	return newUser
+}
