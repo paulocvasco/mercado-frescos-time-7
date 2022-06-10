576b79740f7146e870b4e7f9be6fe377044c2834
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-06-10 14:32:39 -0300

    Revert "Merge pull request #31 from paulocvasco/feature/implement_permanent_db"
    This reverts commit 0ce459189c8646438925404df196fb44a8ef500d, reversing
    changes made to ae4d4312dd01496ebdfb86c71ca1db811a75bcb6.

--- go-web/cmd/server/routes/warehouse.go
+++ go-web/cmd/server/routes/warehouse.go
@@ -3,14 +3,12 @@ package routes
 import (
 	"mercado-frescos-time-7/go-web/cmd/server/controller"
 	"mercado-frescos-time-7/go-web/internal/warehouse"
-	"mercado-frescos-time-7/go-web/pkg/db"
 
 	"github.com/gin-gonic/gin"
 )
 
 func InstanceWarehouse(r *gin.Engine) {
-	database := db.NewDatabase()
-	repository := warehouse.NewRepository(database)
+	repository := warehouse.NewRepository()
 	service := warehouse.NewService(repository)
 	controller := controller.NewControllerWarehouse(service)
 
