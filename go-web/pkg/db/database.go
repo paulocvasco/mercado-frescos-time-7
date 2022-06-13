272596fc25779a54f1562064acef05491da880df
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-06-13 15:42:06 -0300

    Create function to retrieve path

---
+++ go-web/pkg/db/database.go
@@ -0,0 +1,103 @@
+package db
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"mercado-frescos-time-7/go-web/internal/models"
+	"os"
+)
+
+type DB interface {
+	Save(interface{}) error
+	Load(interface{}) error
+}
+
+type database struct{}
+
+func NewDatabase() DB {
+	return &database{}
+}
+
+func (db *database) Save(model interface{}) error {
+
+	fmt.Printf("%+v\n", "kalsdjkasldjkasldjkasdjasjl") // output for debug
+
+	var err error
+	var file *os.File
+
+	defer file.Close()
+
+	path, err := getPath(model)
+	if err != nil {
+		return err
+	}
+	// check if exists a file to save data
+	_, err = os.Stat(path)
+	if err != nil {
+		file, err = os.Create(path)
+		if err != nil {
+			return err
+		}
+	} else {
+		file, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+		if err != nil {
+			return err
+		}
+	}
+
+	data, err := json.Marshal(model)
+	if err != nil {
+		return err
+	}
+	// save data
+	_, err = file.Write(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (db *database) Load(model interface{}) error {
+	path, err := getPath(model)
+	if err != nil {
+		return err
+	}
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(data, &model)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getPath(model interface{}) (string, error) {
+	switch model.(type) {
+	case models.Buyer:
+		return "./buyer.db", nil
+	case models.Employee:
+		return "./employee.db", nil
+	case models.Product:
+		return "./produt.db", nil
+	case models.Section:
+		return "./section.db", nil
+	case models.Seller:
+		return "./seller.db", nil
+	case models.Warehouse:
+		return "./warehouse.db", nil
+	default:
+		return "", errors.New("invalid data")
+	}
+}
