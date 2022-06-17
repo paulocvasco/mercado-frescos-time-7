d93e6ae7418014eb2ed57b034e1e6e31f7306c0d
rodneyems <rodney.martins@live.com>
2022-06-17 16:53:16 -0300

    feat: add getbyid tests

--- go-web/internal/warehouse/warehouse_test.go
+++ go-web/internal/warehouse/warehouse_test.go
@@ -4,13 +4,15 @@ import (
 	"mercado-frescos-time-7/go-web/internal/models"
 	"mercado-frescos-time-7/go-web/internal/warehouse"
 	"mercado-frescos-time-7/go-web/internal/warehouse/mock/mockRepository"
+	customerrors "mercado-frescos-time-7/go-web/pkg/custom_errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestGetAll(t *testing.T) {
-	type getAllResponse struct {
+	type mockResponse struct {
 		data models.Warehouses
 		err  error
 	}
@@ -20,14 +22,14 @@ func TestGetAll(t *testing.T) {
 	}
 	type testData struct {
 		testName     string
-		response     getAllResponse
+		response     mockResponse
 		expectResult getAllExpected
 	}
 
 	testsCases := []testData{
 		{
 			testName: "should return all warehouses",
-			response: getAllResponse{
+			response: mockResponse{
 				data: models.Warehouses{Warehouses: []models.Warehouse{
 					{ID: 0, Address: "foo", Telephone: "foo"},
 					{ID: 1, Address: "foo", Telephone: "foo"},
@@ -46,7 +48,7 @@ func TestGetAll(t *testing.T) {
 		},
 		{
 			testName: "should return error",
-			response: getAllResponse{
+			response: mockResponse{
 				data: models.Warehouses{},
 				err:  nil,
 			},
@@ -65,5 +67,71 @@ func TestGetAll(t *testing.T) {
 
 		assert.Equal(t, test.expectResult.data, response, test.testName)
 		assert.Equal(t, test.expectResult.err, err, test.testName)
+	}
+}
+
+func TestGetById(t *testing.T) {
+	type mockResponse struct {
+		data models.Warehouse
+		err  error
+	}
+	type getByIdExpected struct {
+		data models.Warehouse
+		err  error
+	}
+	type testData struct {
+		testName string
+		mockResponse
+		expectedResult getByIdExpected
+		serviceArg int
+	}
+
+	testsCases := []testData{
+		{
+			testName: "should return warehouse by id",
+			mockResponse: mockResponse{
+				data: models.Warehouse{ID: 1, Address: "foo"},
+				err:  nil,
+			},
+			expectedResult: getByIdExpected{
+				data: models.Warehouse{ID: 1, Address: "foo"},
+				err:  nil,
+			},
+			serviceArg: 1,
+		},
+		{
+			testName: "should return invalid id error",
+			mockResponse: mockResponse{
+				data: models.Warehouse{},
+				err:  customerrors.ErrorInvalidID,
+			},
+			expectedResult: getByIdExpected{
+				data: models.Warehouse{},
+				err:  customerrors.ErrorInvalidID,
+			},
+			serviceArg: 1,
+		},
+		{
+			testName: "should return invalid db error",
+			mockResponse: mockResponse{
+				data: models.Warehouse{},
+				err:  customerrors.ErrorInvalidDB,
+			},
+			expectedResult: getByIdExpected{
+				data: models.Warehouse{},
+				err:  customerrors.ErrorInvalidDB,
+			},
+			serviceArg: 1,
+		},
+	}
+	for _, test := range testsCases {
+		mockRepo := mockRepository.NewRepository(t)
+		serv := warehouse.NewService(mockRepo)
+		mockRepo.On("GetByID", mock.Anything).Return(test.mockResponse.data, test.mockResponse.err)
+
+		response, err := serv.GetByID(test.serviceArg)
+
+		assert.Equal(t, test.expectedResult.data, response, test.testName)
+		assert.Equal(t, test.expectedResult.err, err, test.testName)
 	}
 }
