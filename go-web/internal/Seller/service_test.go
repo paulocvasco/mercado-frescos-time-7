3ddade67f058c0cf68ea67c93feaf8052533c058
jandrade13 <jefferson.jandrade@mercadolivre.com>
2022-06-20 14:30:34 -0300

    feat: created update teste, service

--- go-web/internal/Seller/service_test.go
+++ go-web/internal/Seller/service_test.go
@@ -1,6 +1,7 @@
 package Seller_test
 
 import (
+	"encoding/json"
 	"errors"
 	"mercado-frescos-time-7/go-web/internal/Seller"
 	"mercado-frescos-time-7/go-web/internal/Seller/mocks"
@@ -9,6 +10,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestGetAll(t *testing.T) {
@@ -106,5 +108,182 @@ func TestGetId(t *testing.T) {
 		resp, err := service.GetId(value.params)
 		assert.Equal(t, value.expectResponse, resp, value.message)
 		assert.Equal(t, value.expectError, err, value.message)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	type tests struct {
+		name           string
+		mockResponse   error
+		expectResponse error
+		expectError    error
+		message        string
+		params         int
+	}
+	testCases := []tests{
+		{"Get Id = 1", nil, nil, nil, "Id doesn`t exist", 1},
+		{"Get Id = 4", errors.New("Error"), nil, errors.New("Error"), "Id doesn`t exist", 4},
+	}
+	for _, value := range testCases {
+		mockRepository := mocks.NewRepository(t)
+		service := Seller.NewService(mockRepository)
+		mockRepository.On("Delete", value.params).Return(value.mockResponse, value.expectError)
+		err := service.Delete(value.params)
+		assert.Equal(t, value.expectError, err, value.message)
+	}
+}
+
+func TestStore(t *testing.T) {
+	type tests struct {
+		name           string
+		mockResponse   models.Seller
+		expectResponse models.Seller
+		expectError    error
+		message        string
+		errorLastID    error
+	}
+	response := models.Seller{
+		ID:           1,
+		Cid:          123,
+		Company_name: "Meli1",
+		Address:      "Rua 1",
+		Telephone:    "(11) 33387767",
+	}
+	testCases := []tests{
+		{"Store", response, response, nil, "Errro created", nil},
+		{"Error", models.Seller{}, models.Seller{}, errors.New("Error"), "Errro created", nil},
+		{"Store Error LastID", models.Seller{}, models.Seller{}, errors.New("Error"), "Id doesn`t exist", errors.New("Error")},
+		{"Error LastID", models.Seller{}, models.Seller{}, errors.New("Error"), "Errro created", nil},
+	}
+	for _, value := range testCases {
+		mockRepository := mocks.NewRepository(t)
+		service := Seller.NewService(mockRepository)
+
+		mockRepository.On("LastID").Return((response.ID - 1), value.errorLastID)
+		if value.errorLastID == nil {
+
+			mockRepository.On("Store",
+				response.ID,
+				value.mockResponse.Cid,
+				value.mockResponse.Company_name,
+				value.mockResponse.Address,
+				value.mockResponse.Telephone).
+				Return(value.mockResponse, value.expectError)
+
+			resp, err := service.
+				Store(value.mockResponse.Cid,
+					value.mockResponse.Company_name,
+					value.mockResponse.Address,
+					value.mockResponse.Telephone,
+				)
+			assert.Equal(t, value.expectResponse, resp, value.message)
+			assert.Equal(t, value.expectError, err, value.message)
+
+			t.Skip()
+		}
+		_, err := service.Store(value.mockResponse.Cid, value.mockResponse.Company_name, value.mockResponse.Address, value.mockResponse.Telephone)
+		assert.Error(t, err)
+
+	}
+
+}
+
+func TestUpdate(t *testing.T) {
+	type tests struct {
+		name           string
+		mockResponse   models.Seller
+		expectResponse models.Seller
+		expectError    error
+		message        string
+		getIdError     error
+	}
+	type update struct {
+		Company_name string `json:"company_name"`
+		Address      string `json:"address"`
+		Telephone    string `json:"telephone"`
+	}
+	response := []models.Seller{{
+		ID:           1,
+		Cid:          123,
+		Company_name: "Mercado Livre1",
+		Address:      "Rua 1",
+		Telephone:    "(11) 3333-3333",
+	}, {
+		ID:           1,
+		Cid:          123,
+		Company_name: "Mercado Livre2",
+		Address:      "Rua 1",
+		Telephone:    "(11) 3333-3333",
+	}, {
+		ID:           1,
+		Cid:          123,
+		Company_name: "Mercado Livre2",
+		Address:      "Rua 1",
+		Telephone:    "(11) 3333-3333",
+	}, {
+		ID:           1,
+		Cid:          123,
+		Company_name: "Meli1",
+		Address:      "Rua 1",
+		Telephone:    "(11) 3333-3333",
+	}, {
+		ID:           1,
+		Cid:          123,
+		Company_name: "Mercado Livre1",
+		Address:      "Rua 1",
+		Telephone:    "(11) 33387767",
+	}, {
+		ID:           1,
+		Cid:          123,
+		Company_name: "Meli1",
+		Address:      "Rua 2",
+		Telephone:    "(11) 33387767",
+	}, {
+		ID:           1,
+		Cid:          123,
+		Company_name: "Meli1",
+		Address:      "Rua 1",
+		Telephone:    "(11) 3333-3333",
+	},
+	}
+	// valueUpdate := []update{{
+	// 	Company_name: "Mercado Livre1",
+	// 	Address:      "Rua 1",
+	// 	Telephone:    "(11) 3333-3333",
+	// }, {
+	// 	Company_name: "Mercado Livre2",
+	// 	Address:      "Rua 1",
+	// }, {
+	// 	Address:   "Rua 1",
+	// 	Telephone: "(11) 3333-3333",
+	// }, {
+	// 	Company_name: "Mercado Livre1",
+	// 	Telephone:    "(11) 3333-3333",
+	// }, {
+	// 	Company_name: "Mercado Livre1",
+	// }, {
+	// 	Address: "Rua 1",
+	// }, {
+	// 	Telephone: "(11) 3333-3333",
+	// },
+	// }
+
+	testCases := []tests{
+		{"Update", response[0], response[0], nil, "Errro created", nil},
+	}
+	for _, value := range testCases {
+		mockRepository := mocks.NewRepository(t)
+		service := Seller.NewService(mockRepository)
+
+		mockRepository.On("GetId", value.expectResponse.ID).Return(value.expectResponse.ID, value.getIdError).Maybe()
+		mockRepository.On("Update", mock.Anything, mock.Anything).
+			Return(value.expectResponse, value.expectError).Maybe()
+		mockRepository.On("Checkid", value.expectResponse.Cid).Return(value.mockResponse.Cid, nil).Maybe()
+
+		sellerByte, _ := json.Marshal(value.expectResponse)
+		resp, err := service.Update(sellerByte, value.mockResponse.ID)
+		assert.Equal(t, value.expectResponse, resp, value.message)
+		assert.Equal(t, value.expectError, err, value.message)
+
 	}
 }
