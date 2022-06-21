2623ef3cc522d5a6d3c375f253ae2acaa9b80797
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-06-21 20:40:23 -0300

    Organize mocK

--- go-web/internal/employees/mock.go
+++ go-web/internal/employees/mock.go
@@ -1,6 +1,106 @@
 package employees
 
-// type to simulete repository
+// Service and Repository has the same return value
+// to simplicity use the same response
+var lastIDResponse struct {
+	lastId int
+	err    error
+}
+
+var validationResponse struct {
+	err error
+}
+
+var createResponse struct {
+	model Employee
+	err   error
+}
+
+var getAllResponse struct {
+	modelList []Employee
+	err       error
+}
+
+var deleteResponse struct {
+	err error
+}
+
+var getByIdresponse struct {
+	model Employee
+	err   error
+}
+
+var updateResponse struct {
+	model Employee
+	err   error
+}
+
+func ConfigLastID(lastID int, err error) {
+	lastIDResponse.lastId = lastID
+	lastIDResponse.err = err
+}
+
+func ConfigValidationCard(err error) {
+	validationResponse.err = err
+}
+
+func ConfigCreate(err error) {
+	createResponse.err = err
+}
+
+func ConfigGetAll(all []Employee, err error) {
+	getAllResponse.modelList = all
+	getAllResponse.err = err
+}
+
+func ConfigDelete(err error) {
+	deleteResponse.err = err
+}
+
+func ConfigGetByID(model Employee, err error) {
+	getByIdresponse.model = model
+	getByIdresponse.err = err
+}
+
+func ConfigUpdate(model Employee, err error) {
+	updateResponse.model = model
+	updateResponse.err = err
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                                   MOCK SERVICE                                    //
+///////////////////////////////////////////////////////////////////////////////////////
+
+type mockedService struct{}
+
+func NewMockedService() *mockedService {
+	return &mockedService{}
+}
+
+func (s *mockedService) GetAll() ([]Employee, error) {
+	return getAllResponse.modelList, getAllResponse.err
+}
+
+func (s *mockedService) GetByID(id int) (Employee, error) {
+	return getByIdresponse.model, getByIdresponse.err
+}
+
+func (s *mockedService) Create(card_number_id string, first_name string, last_name string, warehouse_id int) (Employee, error) {
+	return Employee{CardNumberId: card_number_id, FirstName: first_name, LastName: last_name, WareHouseId: warehouse_id}, createResponse.err
+}
+
+func (s *mockedService) Update(e RequestPatch, id int) (Employee, error) {
+	return updateResponse.model, updateResponse.err
+}
+
+func (s *mockedService) Delete(id int) error {
+	return deleteResponse.err
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                                   MOCK REPOSITORY                                 //
+///////////////////////////////////////////////////////////////////////////////////////
+
 type mockedRepository struct{}
 
 func CreateMockRepository() *mockedRepository {
@@ -33,98 +133,4 @@ func (r *mockedRepository) Update(e Employee, id int) (Employee, error) {
 
 func (r *mockedRepository) ValidationCardNumberID(card_number_id string) error {
 	return validationResponse.err
-}
-
-// response to each method
-var lastIDResponse struct {
-	lastId int
-	err    error
-}
-
-func ConfigLastID(lastID int, err error) {
-	lastIDResponse.lastId = lastID
-	lastIDResponse.err = err
-}
-
-var validationResponse struct {
-	err error
-}
-
-func ConfigValidationCard(err error) {
-	validationResponse.err = err
-}
-
-var createResponse struct {
-	model Employee
-	err   error
-}
-
-func ConfigCreate(err error) {
-	createResponse.err = err
-}
-
-var getAllResponse struct {
-	modelList []Employee
-	err       error
-}
-
-func ConfigGetAll(all []Employee, err error) {
-	getAllResponse.modelList = all
-	getAllResponse.err = err
-}
-
-var deleteResponse struct {
-	err error
-}
-
-func ConfigDelete(err error) {
-	deleteResponse.err = err
-}
-
-var getByIdresponse struct {
-	model Employee
-	err   error
-}
-
-func ConfigGetByID(model Employee, err error) {
-	getByIdresponse.model = model
-	getByIdresponse.err = err
-}
-
-var updateResponse struct {
-	err error
-}
-
-func ConfigUpdate(err error) {
-	updateResponse.err = err
-}
-
-///////////////////////////////////////////////////////////////////////////////////////
-//                                   MOCK SERVICE                                    //
-///////////////////////////////////////////////////////////////////////////////////////
-
-type mockedService struct{}
-
-func NewMockedService() *mockedService {
-	return &mockedService{}
-}
-
-func (s *mockedService) GetAll() ([]Employee, error) {
-	return nil, nil
-}
-
-func (s *mockedService) GetByID(id int) (Employee, error) {
-	return Employee{}, nil
-}
-
-func (s *mockedService) Create(card_number_id string, first_name string, last_name string, warehouse_id int) (Employee, error) {
-	return Employee{}, nil
-}
-
-func (s *mockedService) Update(e RequestPatch, id int) (Employee, error) {
-	return Employee{}, nil
-}
-
-func (s *mockedService) Delete(id int) error {
-	return nil
 }
