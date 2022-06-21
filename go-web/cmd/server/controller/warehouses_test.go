f2f3655732800841ed6538104a99e0edb38a49ae
rodneyems <rodney.martins@live.com>
2022-06-20 22:45:11 -0300

    feat: add getbyid controller tests

--- go-web/cmd/server/controller/warehouses_test.go
+++ go-web/cmd/server/controller/warehouses_test.go
@@ -2,6 +2,7 @@ package controller_test
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"mercado-frescos-time-7/go-web/cmd/server/controller"
 	"mercado-frescos-time-7/go-web/internal/models"
@@ -14,6 +15,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestGetAllWarehouse(t *testing.T) {
@@ -80,6 +82,95 @@ func TestGetAllWarehouse(t *testing.T) {
 
 		if w.Result().StatusCode < 300 {
 			res := models.Warehouses{}
+			json.Unmarshal(body, &res)
+
+			assert.Equal(t, test.expectResult.statusCode, w.Result().StatusCode, test.testName)
+			assert.Equal(t, test.expectResult.data, res, test.testName)
+		} else {
+			res := web.Response{}
+			json.Unmarshal(body, &res)
+
+			assert.Equal(t, test.expectResult.statusCode, w.Result().StatusCode, test.testName)
+			assert.Equal(t, test.expectResult.data, res, test.testName)
+		}
+	}
+}
+
+func TestGetByIDWarehouse(t *testing.T) {
+	type responseServiceMock struct {
+		data models.Warehouse
+		err  error
+	}
+	type expectResult struct {
+		data       interface{}
+		statusCode int
+	}
+	type testData struct {
+		testName string
+		responseServiceMock
+		expectResult
+		requestedId string
+	}
+	testCases := []testData{
+		{
+			testName: "should return status 200 and a warehouse with correct id",
+			responseServiceMock: responseServiceMock{
+				data: models.Warehouse{
+					ID: 1, Address: "foo", Telephone: "foo", WarehouseCode: "foo", MinimunCapacity: 20, MinimunTemperature: 20,
+				},
+				err: nil,
+			},
+			expectResult: expectResult{
+				data: models.Warehouse{
+					ID: 1, Address: "foo", Telephone: "foo", WarehouseCode: "foo", MinimunCapacity: 20, MinimunTemperature: 20,
+				},
+				statusCode: 200,
+			},
+			requestedId: "1",
+		},
+		{
+			testName: "should return status 404 - invalid id",
+			responseServiceMock: responseServiceMock{
+				data: models.Warehouse{},
+				err:  customerrors.ErrorInvalidID,
+			},
+			expectResult: expectResult{
+				data:       web.Response{Code: "404", Error: customerrors.ErrorInvalidID.Error()},
+				statusCode: 404,
+			},
+			requestedId: "1",
+		},
+		{
+			testName: "should return status 500 - invalid id - ALTERAR ESSE ERRO DPS",
+			responseServiceMock: responseServiceMock{
+				data: models.Warehouse{},
+				err:  customerrors.ErrorInvalidID,
+			},
+			expectResult: expectResult{
+				data:       web.Response{Code: "500", Error: "internal error"},
+				statusCode: 500,
+			},
+			requestedId: "A",
+		},
+	}
+	for _, test := range testCases {
+		gin.SetMode(gin.TestMode)
+
+		mockServ := mockWarehouse.NewService(t)
+		ctrl := controller.NewControllerWarehouse(mockServ)
+		mockServ.On("GetByID", mock.Anything).Return(test.responseServiceMock.data, test.responseServiceMock.err).Maybe()
+
+		w := httptest.NewRecorder()
+		_, router := gin.CreateTestContext(w)
+
+		req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/%v", test.requestedId), nil)
+		router.GET("/:id", ctrl.GetByIDWarehouse)
+		router.ServeHTTP(w, req)
+
+		body, _ := ioutil.ReadAll(w.Body)
+
+		if w.Result().StatusCode < 300 {
+			res := models.Warehouse{}
 			json.Unmarshal(body, &res)
 
 			assert.Equal(t, test.expectResult.statusCode, w.Result().StatusCode, test.testName)
