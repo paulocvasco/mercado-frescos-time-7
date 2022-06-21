6eb549efdf2dbb295bb2f51c13784a0f20ea5fa9
Sebastião Fortes <sebastiaofortes4@gmail.com>
2022-06-21 09:14:42 -0300

    corrigindo identacao

--- go-web/internal/sections/controller_test.go
+++ go-web/internal/sections/controller_test.go
@@ -22,35 +22,35 @@ import (
 )
 
 func TestControllerCreateOK(t *testing.T) {
-serv := mockService.NewService(t)
-contr := controller.NewController(serv)
-r := gin.Default()
-r.POST("/section", contr.Store)
-mysec := sections.Section{
-	ID:                 2,
-	SectionNumber:      1,
-	CurrentTemperature: 1,
-	MinimumTemperature: 1,
-	CurrentCapacity:    1,
-	MinimumCapacity:    1,
-	MaximumCapacity:    1,
-	WarehouseId:        1,
-	ProductTypeId:      1,
-}
-jsonValue, _ := json.Marshal(mysec)
-req, err := http.NewRequest("POST", "/section",bytes.NewBuffer(jsonValue)) 
-if err != nil {
-	fmt.Println(err)
-	return
-}
-w := httptest.NewRecorder()
-serv.On("Store", mock.Anything).Return(mysec, nil)
-r.ServeHTTP(w, req)
-responseData, _ := ioutil.ReadAll(w.Body)
-var result sections.Section
-json.Unmarshal(responseData, &result)
-assert.Equal(t, mysec, result)
-assert.Equal(t, 201, w.Code)
+	serv := mockService.NewService(t)
+	contr := controller.NewController(serv)
+	r := gin.Default()
+	r.POST("/section", contr.Store)
+	mysec := sections.Section{
+		ID:                 2,
+		SectionNumber:      1,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+	jsonValue, _ := json.Marshal(mysec)
+	req, err := http.NewRequest("POST", "/section", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	w := httptest.NewRecorder()
+	serv.On("Store", mock.Anything).Return(mysec, nil)
+	r.ServeHTTP(w, req)
+	responseData, _ := ioutil.ReadAll(w.Body)
+	var result sections.Section
+	json.Unmarshal(responseData, &result)
+	assert.Equal(t, mysec, result)
+	assert.Equal(t, 201, w.Code)
 }
 
 func TestControllerCreateFail(t *testing.T) {
@@ -77,67 +77,67 @@ func TestControllerCreateFail(t *testing.T) {
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 	assert.Equal(t, 422, w.Code)
-	}
+}
 
-	func TestControllerCreateConflict(t *testing.T) {
-		serv := mockService.NewService(t)
-		contr := controller.NewController(serv)
-		r := gin.Default()
-		r.POST("/section", contr.Store)
-		mysec := sections.Section{
-			ID:                 2,
-			SectionNumber:      1,
-			CurrentTemperature: 1,
-			MinimumTemperature: 1,
-			CurrentCapacity:    1,
-			MinimumCapacity:    1,
-			MaximumCapacity:    1,
-			WarehouseId:        1,
-			ProductTypeId:      1,
-		}
-		jsonValue, _ := json.Marshal(mysec)
-		req, err := http.NewRequest("POST", "/section", bytes.NewBuffer(jsonValue))
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		w := httptest.NewRecorder()
-		serv.On("Store", mock.Anything).Return(sections.Section{}, customErrors.ErrorConflict)
-		r.ServeHTTP(w, req)
-		assert.Equal(t, 409, w.Code)
-		}
+func TestControllerCreateConflict(t *testing.T) {
+	serv := mockService.NewService(t)
+	contr := controller.NewController(serv)
+	r := gin.Default()
+	r.POST("/section", contr.Store)
+	mysec := sections.Section{
+		ID:                 2,
+		SectionNumber:      1,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+	jsonValue, _ := json.Marshal(mysec)
+	req, err := http.NewRequest("POST", "/section", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	w := httptest.NewRecorder()
+	serv.On("Store", mock.Anything).Return(sections.Section{}, customErrors.ErrorConflict)
+	r.ServeHTTP(w, req)
+	assert.Equal(t, 409, w.Code)
+}
 
 func TestControllerFindAll(t *testing.T) {
-serv := mockService.NewService(t)
-contr := controller.NewController(serv)
-r := gin.Default()
-r.GET("/section", contr.GetAll)
-mysec := []models.Section{{
-	ID:                 2,
-	SectionNumber:      1,
-	CurrentTemperature: 1,
-	MinimumTemperature: 1,
-	CurrentCapacity:    1,
-	MinimumCapacity:    1,
-	MaximumCapacity:    1,
-	WarehouseId:        1,
-	ProductTypeId:      1,
-},
-}
-jsonValue, _ := json.Marshal(mysec)
-req, err := http.NewRequest("GET", "/section", bytes.NewBuffer(jsonValue))
-if err != nil {
-	fmt.Println(err)
-	return
-}
-w := httptest.NewRecorder()
-serv.On("GetAll").Return(mysec, nil) 
-r.ServeHTTP(w, req)
-responseData, _ := ioutil.ReadAll(w.Body)
-var result []models.Section
-json.Unmarshal(responseData, &result)
-assert.Equal(t, mysec, result)
-assert.Equal(t, 200, w.Code)
+	serv := mockService.NewService(t)
+	contr := controller.NewController(serv)
+	r := gin.Default()
+	r.GET("/section", contr.GetAll)
+	mysec := []models.Section{{
+		ID:                 2,
+		SectionNumber:      1,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	},
+	}
+	jsonValue, _ := json.Marshal(mysec)
+	req, err := http.NewRequest("GET", "/section", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	w := httptest.NewRecorder()
+	serv.On("GetAll").Return(mysec, nil)
+	r.ServeHTTP(w, req)
+	responseData, _ := ioutil.ReadAll(w.Body)
+	var result []models.Section
+	json.Unmarshal(responseData, &result)
+	assert.Equal(t, mysec, result)
+	assert.Equal(t, 200, w.Code)
 }
 
 func TestControllerFindByIDNE(t *testing.T) {
@@ -152,122 +152,122 @@ func TestControllerFindByIDNE(t *testing.T) {
 		return
 	}
 	w := httptest.NewRecorder()
-	serv.On("GetById", mock.Anything).Return(models.Section{}, customErrors.ErrorSectionNotFound) 
+	serv.On("GetById", mock.Anything).Return(models.Section{}, customErrors.ErrorSectionNotFound)
 	r.ServeHTTP(w, req)
 
 	assert.Equal(t, 404, w.Code)
+}
+
+func TestControllerFindByID(t *testing.T) {
+	serv := mockService.NewService(t)
+	contr := controller.NewController(serv)
+	r := gin.Default()
+	r.GET("/section/:id", contr.GetById)
+	mysec := models.Section{
+		ID:                 2,
+		SectionNumber:      1,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
 	}
+	req, err := http.NewRequest("GET", "/section/1", nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	w := httptest.NewRecorder()
+	serv.On("GetById", mock.Anything).Return(mysec, nil)
+	r.ServeHTTP(w, req)
+	var result models.Section
+	responseData, _ := ioutil.ReadAll(w.Body)
+	json.Unmarshal(responseData, &result)
+	assert.Equal(t, mysec, result)
+	assert.Equal(t, 200, w.Code)
+}
 
-	func TestControllerFindByID(t *testing.T) {
-		serv := mockService.NewService(t)
-		contr := controller.NewController(serv)
-		r := gin.Default()
-		r.GET("/section/:id", contr.GetById)
-		mysec := models.Section{
-			ID:                 2,
-			SectionNumber:      1,
-			CurrentTemperature: 1,
-			MinimumTemperature: 1,
-			CurrentCapacity:    1,
-			MinimumCapacity:    1,
-			MaximumCapacity:    1,
-			WarehouseId:        1,
-			ProductTypeId:      1,
-		}
-		req, err := http.NewRequest("GET", "/section/1", nil)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		w := httptest.NewRecorder()
-		serv.On("GetById", mock.Anything).Return(mysec, nil) 
-		r.ServeHTTP(w, req)
-		var result models.Section
-		responseData, _ := ioutil.ReadAll(w.Body)
-		json.Unmarshal(responseData, &result)
-		assert.Equal(t, mysec, result)
-		assert.Equal(t, 200, w.Code)
-		}
+func TestControllerUpdateNE(t *testing.T) {
+	serv := mockService.NewService(t)
+	contr := controller.NewController(serv)
+	r := gin.Default()
+	r.PATCH("/section/:id", contr.Update)
+	mysec := models.Section{
+		ID:                 2,
+		SectionNumber:      1,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+	jsonValue, _ := json.Marshal(mysec)
+	req, err := http.NewRequest("PATCH", "/section/1", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	w := httptest.NewRecorder()
+	serv.On("Update", mock.Anything, mock.Anything).Return(sections.Section{}, customErrors.ErrorInvalidID)
+	r.ServeHTTP(w, req)
 
-		func TestControllerUpdateNE(t *testing.T) {
-			serv := mockService.NewService(t)
-			contr := controller.NewController(serv)
-			r := gin.Default()
-			r.PATCH("/section/:id", contr.Update)
-			mysec := models.Section{
-				ID:                 2,
-				SectionNumber:      1,
-				CurrentTemperature: 1,
-				MinimumTemperature: 1,
-				CurrentCapacity:    1,
-				MinimumCapacity:    1,
-				MaximumCapacity:    1,
-				WarehouseId:        1,
-				ProductTypeId:      1,
-			}
-			jsonValue, _ := json.Marshal(mysec)
-			req, err := http.NewRequest("PATCH", "/section/1", bytes.NewBuffer(jsonValue))
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			w := httptest.NewRecorder()
-			serv.On("Update", mock.Anything, mock.Anything).Return(sections.Section{}, customErrors.ErrorInvalidID)
-			r.ServeHTTP(w, req)
-		
-			assert.Equal(t, 404, w.Code)
-			}
+	assert.Equal(t, 404, w.Code)
+}
 
-			func TestControlleDeleteNE(t *testing.T) {
-				serv := mockService.NewService(t)
-				contr := controller.NewController(serv)
-				r := gin.Default()
-				r.DELETE("/section/:id", contr.Delete)
-				mysec := models.Section{
-					ID:                 2,
-					SectionNumber:      1,
-					CurrentTemperature: 1,
-					MinimumTemperature: 1,
-					CurrentCapacity:    1,
-					MinimumCapacity:    1,
-					MaximumCapacity:    1,
-					WarehouseId:        1,
-					ProductTypeId:      1,
-				}
-				jsonValue, _ := json.Marshal(mysec)
-				req, err := http.NewRequest("DELETE", "/section/1", bytes.NewBuffer(jsonValue))
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-				w := httptest.NewRecorder()
-				serv.On("Delete", mock.Anything).Return(customErrors.ErrorInvalidID)
-				r.ServeHTTP(w, req)
-			
-				assert.Equal(t, 404, w.Code)
-				}
+func TestControlleDeleteNE(t *testing.T) {
+	serv := mockService.NewService(t)
+	contr := controller.NewController(serv)
+	r := gin.Default()
+	r.DELETE("/section/:id", contr.Delete)
+	mysec := models.Section{
+		ID:                 2,
+		SectionNumber:      1,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+	jsonValue, _ := json.Marshal(mysec)
+	req, err := http.NewRequest("DELETE", "/section/1", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	w := httptest.NewRecorder()
+	serv.On("Delete", mock.Anything).Return(customErrors.ErrorInvalidID)
+	r.ServeHTTP(w, req)
 
-				func TestControlleDeleteOK(t *testing.T) {
-					serv := mockService.NewService(t)
-					contr := controller.NewController(serv)
-					r := gin.Default()
-					r.DELETE("/section/:id", contr.Delete)
-					mysec := models.Section{
-						ID:                 2,
-						SectionNumber:      1,
-						CurrentTemperature: 1,
-						MinimumTemperature: 1,
-						CurrentCapacity:    1,
-						MinimumCapacity:    1,
-						MaximumCapacity:    1,
-						WarehouseId:        1,
-						ProductTypeId:      1,
-					}
-					jsonValue, _ := json.Marshal(mysec)
-					req, _ := http.NewRequest("DELETE", "/section/1", bytes.NewBuffer(jsonValue))
-					w := httptest.NewRecorder()
-					serv.On("Delete", mock.Anything).Return(nil)
-					r.ServeHTTP(w, req)
-				
-					assert.Equal(t, 204, w.Code)
-					}
+	assert.Equal(t, 404, w.Code)
+}
+
+func TestControlleDeleteOK(t *testing.T) {
+	serv := mockService.NewService(t)
+	contr := controller.NewController(serv)
+	r := gin.Default()
+	r.DELETE("/section/:id", contr.Delete)
+	mysec := models.Section{
+		ID:                 2,
+		SectionNumber:      1,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+	jsonValue, _ := json.Marshal(mysec)
+	req, _ := http.NewRequest("DELETE", "/section/1", bytes.NewBuffer(jsonValue))
+	w := httptest.NewRecorder()
+	serv.On("Delete", mock.Anything).Return(nil)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 204, w.Code)
+}
--- go-web/internal/sections/service_test.go
+++ go-web/internal/sections/service_test.go
@@ -15,170 +15,169 @@ import (
 
 func TestCreateOK(t *testing.T) {
 	repo := mockRepository.NewRepository(t)
-    serv := sections.NewService(repo)
-    mysec := sections.Section{
-        ID:                 1,
-        SectionNumber:      1,
-        CurrentTemperature: 1,
-        MinimumTemperature: 1,
-        CurrentCapacity:    1,
-        MinimumCapacity:    1,
-        MaximumCapacity:    1,
-        WarehouseId:        1,
-        ProductTypeId:      1,
-    }
+	serv := sections.NewService(repo)
+	mysec := sections.Section{
+		ID:                 1,
+		SectionNumber:      1,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
 
 	mbytes, _ := json.Marshal(mysec)
 
-    repo.On("Store", mock.Anything).Return(mysec, nil)
+	repo.On("Store", mock.Anything).Return(mysec, nil)
 	repo.On("VerifySectionNumber", mock.Anything).Return(nil)
-    result, _ := serv.Store(mbytes)
-    assert.Equal(t, mysec, result)
+	result, _ := serv.Store(mbytes)
+	assert.Equal(t, mysec, result)
 }
 
 func TestCreateFailConflict(t *testing.T) {
-    repo := mockRepository.NewRepository(t)
-    serv := sections.NewService(repo)
-    mysec1 := models.Section{
-        ID:                 1,
-        SectionNumber:      1,
-        CurrentTemperature: 1,
-        MinimumTemperature: 1,
-        CurrentCapacity:    1,
-        MinimumCapacity:    1,
-        MaximumCapacity:    1,
-        WarehouseId:        1,
-        ProductTypeId:      1,
-    }
+	repo := mockRepository.NewRepository(t)
+	serv := sections.NewService(repo)
+	mysec1 := models.Section{
+		ID:                 1,
+		SectionNumber:      1,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
 
 	mbytes, _ := json.Marshal(mysec1)
 
-    repo.On("VerifySectionNumber", mock.Anything).Return(customErrors.ErrorConflict)
-    _, err := serv.Store(mbytes)
-    assert.Equal(t, customErrors.ErrorConflict, err,)
+	repo.On("VerifySectionNumber", mock.Anything).Return(customErrors.ErrorConflict)
+	_, err := serv.Store(mbytes)
+	assert.Equal(t, customErrors.ErrorConflict, err)
 }
 
 func TestFindAll(t *testing.T) {
-    repo := mockRepository.NewRepository(t)
-    serv := sections.NewService(repo)
-    stub := []models.Section{   
-            {
-                ID:                 1,
-                SectionNumber:      1,
-                CurrentTemperature: 1,
-                MinimumTemperature: 1,
-                CurrentCapacity:    -1,
-                MinimumCapacity:    1,
-                MaximumCapacity:    1,
-                WarehouseId:        1,
-                ProductTypeId:      1,
-            },
-            {
-                ID:                 2,
-                SectionNumber:      1,
-                CurrentTemperature: 1,
-                MinimumTemperature: 1,
-                CurrentCapacity:    -1,
-                MinimumCapacity:    1,
-                MaximumCapacity:    1,
-                WarehouseId:        1,
-                ProductTypeId:      1,
-            },
-    }
+	repo := mockRepository.NewRepository(t)
+	serv := sections.NewService(repo)
+	stub := []models.Section{
+		{
+			ID:                 1,
+			SectionNumber:      1,
+			CurrentTemperature: 1,
+			MinimumTemperature: 1,
+			CurrentCapacity:    -1,
+			MinimumCapacity:    1,
+			MaximumCapacity:    1,
+			WarehouseId:        1,
+			ProductTypeId:      1,
+		},
+		{
+			ID:                 2,
+			SectionNumber:      1,
+			CurrentTemperature: 1,
+			MinimumTemperature: 1,
+			CurrentCapacity:    -1,
+			MinimumCapacity:    1,
+			MaximumCapacity:    1,
+			WarehouseId:        1,
+			ProductTypeId:      1,
+		},
+	}
 
-    repo.On("GetAll", mock.Anything).Return(stub, nil)
-    result, _ := serv.GetAll()
-    assert.Equal(t, stub, result,)
+	repo.On("GetAll", mock.Anything).Return(stub, nil)
+	result, _ := serv.GetAll()
+	assert.Equal(t, stub, result)
 }
 
-func TestFindByIdNE(t *testing.T){
-    repo := mockRepository.NewRepository(t)
-    serv := sections.NewService(repo)
+func TestFindByIdNE(t *testing.T) {
+	repo := mockRepository.NewRepository(t)
+	serv := sections.NewService(repo)
 
-    repo.On("GetById", mock.Anything).Return(models.Section{}, customErrors.ErrorInvalidID)
-    
-    _, resultError := serv.GetById("1")
-    assert.Equal(t,customErrors.ErrorInvalidID, resultError,)
+	repo.On("GetById", mock.Anything).Return(models.Section{}, customErrors.ErrorInvalidID)
+
+	_, resultError := serv.GetById("1")
+	assert.Equal(t, customErrors.ErrorInvalidID, resultError)
 
 }
 
 func TestUpdate(t *testing.T) {
-    repo := mockRepository.NewRepository(t)
-    serv := sections.NewService(repo)
-    mysec := models.Section{
-        ID:                 1,
-        SectionNumber:      1,
-        CurrentTemperature: 1,
-        MinimumTemperature: 1,
-        CurrentCapacity:    1,
-        MinimumCapacity:    1,
-        MaximumCapacity:    1,
-        WarehouseId:        1,
-        ProductTypeId:      1,
-    }
-    umysec := sections.Section{
-        ID:                 1,
-        SectionNumber:      2,
-        CurrentTemperature: 1,
-        MinimumTemperature: 1,
-        CurrentCapacity:    1,
-        MinimumCapacity:    1,
-        MaximumCapacity:    1,
-        WarehouseId:        1,
-        ProductTypeId:      1,
-    }
+	repo := mockRepository.NewRepository(t)
+	serv := sections.NewService(repo)
+	mysec := models.Section{
+		ID:                 1,
+		SectionNumber:      1,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+	umysec := sections.Section{
+		ID:                 1,
+		SectionNumber:      2,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
 
 	//Existe uma conversão do objeto inicial models.Section para []bytes (ln. 105) e depois para sections.Section (ln. 112)
 
-    repo.On("Update", mock.Anything, mock.Anything).Return(nil)
-    repo.On("GetById", mock.Anything).Return(mysec, nil)
-    repo.On("ValidateID", mock.Anything).Return(true)
+	repo.On("Update", mock.Anything, mock.Anything).Return(nil)
+	repo.On("GetById", mock.Anything).Return(mysec, nil)
+	repo.On("ValidateID", mock.Anything).Return(true)
 	mbytes, _ := json.Marshal(umysec)
-    result, _ := serv.Update("2", mbytes)
-    assert.Equal(t, umysec, result,)
+	result, _ := serv.Update("2", mbytes)
+	assert.Equal(t, umysec, result)
 }
 
 func TestUpdateNE(t *testing.T) {
-    repo := mockRepository.NewRepository(t)
-    serv := sections.NewService(repo)
+	repo := mockRepository.NewRepository(t)
+	serv := sections.NewService(repo)
 
-    umysec := models.Section{
-        ID:                 1,
-        SectionNumber:      0,
-        CurrentTemperature: 1,
-        MinimumTemperature: 1,
-        CurrentCapacity:    0,
-        MinimumCapacity:    1,
-        MaximumCapacity:    1,
-        WarehouseId:        1,
-        ProductTypeId:      1,
-    }
+	umysec := models.Section{
+		ID:                 1,
+		SectionNumber:      0,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    0,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
 
-    repo.On("GetById", mock.Anything).Return(models.Section{}, errors.New("erro simulado"))
-    repo.On("ValidateID", mock.Anything).Return(true)
+	repo.On("GetById", mock.Anything).Return(models.Section{}, errors.New("erro simulado"))
+	repo.On("ValidateID", mock.Anything).Return(true)
 	mbytes, _ := json.Marshal(umysec)
-    _, err := serv.Update("2", mbytes)
-    assert.Equal(t, customErrors.ErrorInvalidID, err,)
+	_, err := serv.Update("2", mbytes)
+	assert.Equal(t, customErrors.ErrorInvalidID, err)
 }
 
 func TestDeleteNE(t *testing.T) {
-    repo := mockRepository.NewRepository(t)
-    serv := sections.NewService(repo)
-    repo.On("GetById", mock.Anything).Return(models.Section{}, customErrors.ErrorInvalidID)
-    
-    _, resultError := serv.GetById("1")
-    assert.Equal(t,customErrors.ErrorInvalidID, resultError,)
-    
+	repo := mockRepository.NewRepository(t)
+	serv := sections.NewService(repo)
+	repo.On("GetById", mock.Anything).Return(models.Section{}, customErrors.ErrorInvalidID)
+
+	_, resultError := serv.GetById("1")
+	assert.Equal(t, customErrors.ErrorInvalidID, resultError)
+
 }
 
 func TestDEeleteOK(t *testing.T) {
-    repo := mockRepository.NewRepository(t)
-    serv := sections.NewService(repo)
-    repo.On("GetById", mock.Anything).Return(models.Section{}, nil)
-    repo.On("ValidateID", mock.Anything).Return(true)
-    repo.On("Delete", mock.Anything).Return(nil)
-    resultError := serv.Delete("1")
-    assert.Equal(t,nil, resultError,)
+	repo := mockRepository.NewRepository(t)
+	serv := sections.NewService(repo)
+	repo.On("GetById", mock.Anything).Return(models.Section{}, nil)
+	repo.On("ValidateID", mock.Anything).Return(true)
+	repo.On("Delete", mock.Anything).Return(nil)
+	resultError := serv.Delete("1")
+	assert.Equal(t, nil, resultError)
 
-    
 }
