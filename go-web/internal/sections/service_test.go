34abea79f1fabdb83b4d14559c7dc4a5639aec34
Sebastião Fortes <sebastiaofortes4@gmail.com>
2022-06-23 14:34:04 -0300

    completando cobertura do service 92%

--- go-web/internal/sections/service_test.go
+++ go-web/internal/sections/service_test.go
@@ -190,6 +190,29 @@ func TestCreateFailConflict(t *testing.T) {
 	assert.Equal(t, customErrors.ErrorConflict, err)
 }
 
+func TestCreateFailStore(t *testing.T) {
+	repo := mockRepository.NewRepository(t)
+	serv := sections.NewService(repo)
+	mysec1 := models.Section{
+		ID:                 1,
+		SectionNumber:      1,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+
+	mbytes, _ := json.Marshal(mysec1)
+
+	repo.On("VerifySectionNumber", mock.Anything).Return(nil)
+	repo.On("Store", mock.Anything).Return(sections.Section{}, customErrors.ErrorStoreFailed)
+	_, err := serv.Store(mbytes)
+	assert.Equal(t, customErrors.ErrorStoreFailed, err)
+}
+
 func TestCreateFailUnMarshal(t *testing.T) {
 	repo := mockRepository.NewRepository(t)
 	serv := sections.NewService(repo)
@@ -319,6 +342,150 @@ func TestUpdate(t *testing.T) {
 	assert.Equal(t, umysec, result)
 }
 
+func TestUpdateErrorSectionNumber(t *testing.T) {
+	repo := mockRepository.NewRepository(t)
+	serv := sections.NewService(repo)
+	umysec := sections.Section{
+		ID:                 1,
+		SectionNumber:      -2,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+
+	//Existe uma conversão do objeto inicial models.Section para []bytes (ln. 105) e depois para sections.Section (ln. 112)
+
+	repo.On("GetById", mock.Anything).Return(models.Section{}, nil)
+	repo.On("ValidateID", mock.Anything).Return(true)
+	mbytes, _ := json.Marshal(umysec)
+	_, err:= serv.Update("2", mbytes)
+	assert.Equal(t, customErrors.ErrorSectionNumber, err)
+}
+
+func TestUpdateErrorCurrentCapacity(t *testing.T) {
+	repo := mockRepository.NewRepository(t)
+	serv := sections.NewService(repo)
+	umysec := sections.Section{
+		ID:                 1,
+		SectionNumber:      2,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    -1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+
+	//Existe uma conversão do objeto inicial models.Section para []bytes (ln. 105) e depois para sections.Section (ln. 112)
+
+	repo.On("GetById", mock.Anything).Return(models.Section{}, nil)
+	repo.On("ValidateID", mock.Anything).Return(true)
+	mbytes, _ := json.Marshal(umysec)
+	_, err:= serv.Update("2", mbytes)
+	assert.Equal(t, customErrors.ErrorCurrentCapacity, err)
+}
+
+func TestUpdateErrorMinimumCapacity(t *testing.T) {
+	repo := mockRepository.NewRepository(t)
+	serv := sections.NewService(repo)
+	umysec := sections.Section{
+		ID:                 1,
+		SectionNumber:      2,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    1,
+		MinimumCapacity:    -1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+
+	//Existe uma conversão do objeto inicial models.Section para []bytes (ln. 105) e depois para sections.Section (ln. 112)
+
+	repo.On("GetById", mock.Anything).Return(models.Section{}, nil)
+	repo.On("ValidateID", mock.Anything).Return(true)
+	mbytes, _ := json.Marshal(umysec)
+	_, err:= serv.Update("2", mbytes)
+	assert.Equal(t, customErrors.ErrorMinimumCapacity, err)
+}
+
+func TestUpdateErrorMaximumCapacity(t *testing.T) {
+	repo := mockRepository.NewRepository(t)
+	serv := sections.NewService(repo)
+	umysec := sections.Section{
+		ID:                 1,
+		SectionNumber:      2,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    -1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+
+	//Existe uma conversão do objeto inicial models.Section para []bytes (ln. 105) e depois para sections.Section (ln. 112)
+
+	repo.On("GetById", mock.Anything).Return(models.Section{}, nil)
+	repo.On("ValidateID", mock.Anything).Return(true)
+	mbytes, _ := json.Marshal(umysec)
+	_, err:= serv.Update("2", mbytes)
+	assert.Equal(t, customErrors.ErrorMaximumCapacity, err)
+}
+
+func TestUpdateErrorWarehouseID(t *testing.T) {
+	repo := mockRepository.NewRepository(t)
+	serv := sections.NewService(repo)
+	umysec := sections.Section{
+		ID:                 1,
+		SectionNumber:      2,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        -1,
+		ProductTypeId:      1,
+	}
+
+	//Existe uma conversão do objeto inicial models.Section para []bytes (ln. 105) e depois para sections.Section (ln. 112)
+
+	repo.On("GetById", mock.Anything).Return(models.Section{}, nil)
+	repo.On("ValidateID", mock.Anything).Return(true)
+	mbytes, _ := json.Marshal(umysec)
+	_, err:= serv.Update("2", mbytes)
+	assert.Equal(t, customErrors.ErrorWarehouseID, err)
+}
+
+func TestUpdateErrorProductTypeID(t *testing.T) {
+	repo := mockRepository.NewRepository(t)
+	serv := sections.NewService(repo)
+	umysec := sections.Section{
+		ID:                 1,
+		SectionNumber:      2,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      -1,
+	}
+
+	//Existe uma conversão do objeto inicial models.Section para []bytes (ln. 105) e depois para sections.Section (ln. 112)
+
+	repo.On("GetById", mock.Anything).Return(models.Section{}, nil)
+	repo.On("ValidateID", mock.Anything).Return(true)
+	mbytes, _ := json.Marshal(umysec)
+	_, err:= serv.Update("2", mbytes)
+	assert.Equal(t, customErrors.ErrorProductTypeID, err)
+}
+
 func TestUpdateNE(t *testing.T) {
 	repo := mockRepository.NewRepository(t)
 	serv := sections.NewService(repo)
@@ -339,6 +506,35 @@ func TestUpdateNE(t *testing.T) {
 	repo.On("ValidateID", mock.Anything).Return(true)
 	mbytes, _ := json.Marshal(umysec)
 	_, err := serv.Update("2", mbytes)
+	assert.Equal(t, customErrors.ErrorInvalidID, err)
+}
+
+func TestUpdateErrorValidateID(t *testing.T) {
+	repo := mockRepository.NewRepository(t)
+	serv := sections.NewService(repo)
+
+	umysec := models.Section{
+		ID:                 1,
+		SectionNumber:      0,
+		CurrentTemperature: 1,
+		MinimumTemperature: 1,
+		CurrentCapacity:    0,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+	repo.On("ValidateID", mock.Anything).Return(false)
+	mbytes, _ := json.Marshal(umysec)
+	_, err := serv.Update("2", mbytes)
+	assert.Equal(t, customErrors.ErrorInvalidID, err)
+}
+
+func TestUpdateFailID(t *testing.T) {
+	repo := mockRepository.NewRepository(t)
+	serv := sections.NewService(repo)
+	_, err := serv.Update("s", nil)
+	//a funcao verifica se o erro é nil porque o service retorna nil para esses caso de erro
 	assert.Equal(t, customErrors.ErrorInvalidID, err)
 }
 
