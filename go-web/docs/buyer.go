3df0abb17322b7c88095170c2af8b61233bf0689
jandrade13 <jefferson.jandrade@mercadolivre.com>
2022-06-27 12:08:23 -0300

    created documentation buyers

---
+++ go-web/docs/buyer.go
@@ -0,0 +1,123 @@
+package docs
+
+import (
+	"mercado-frescos-time-7/go-web/internal/models"
+)
+
+/////////////////////////////////////////////////////////////////////////////////////
+//////////////////               END POINTS               ///////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+// swagger:route GET /buyers/{id} Buyer GetBuyerID
+// Get a buyer from db.
+// responses:
+//    200: buyerIDResponse
+//    404: errorResponse
+//    500: errorServerResponse
+
+// swagger:route GET /buyers/ Buyer GetAll
+// Get all objects stored on db.
+// responses:
+//    200: buyerAll
+//    404: errorResponse
+//    500: errorServerResponse
+
+// swagger:route POST /buyers/ Buyer createBuyer
+// Add a new object on db.
+// responses:
+//    201: buyerIDResponse
+//    422: errorResponse
+//    500: errorServerResponse
+
+// swagger:route DELETE /buyers/{id} Buyer deleteBuyerID
+// Remove a corresponding ID object from db.
+// responses:
+//    204: description: OK
+//    404: errorResponse
+//    500: errorServerResponse
+
+// swagger:route PATCH /buyers/{id} Buyer updatebuyer
+// Edit an object on db.
+// responses:
+//    200: buyerIDResponse
+//    404: errorResponse
+//    422: errorResponse
+//    500: errorServerResponse
+
+/////////////////////////////////////////////////////////////////////////////////////
+//////////////////                  RESPONSES               /////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+// Corresponding object from db on json format.
+// swagger:response buyerIDResponse
+type buyerIDResponse struct {
+	//in: body
+	data struct {
+		Code string       `json:"code"`
+		Data models.Buyer `json:"data"`
+	}
+}
+
+// Corresponding object from db on json format.
+// swagger:response buyerAll
+type buyerAllResponse struct {
+	//in: body
+	data struct {
+		Code string        `json:"code"`
+		Data models.Buyers `json:"data"`
+	}
+}
+
+// Error message has the returned code and a descripton to help understand the cause.
+// swagger:response errorResponse
+type errorResponseBuyer struct {
+	// in: body
+	data struct {
+		Code    string `json:"code"`
+		Message string `json:"error"`
+	}
+}
+
+// Error has the returned when exists a server error.
+// swagger:response errorServerResponse
+type errorServerResponseBuyer struct {
+	// in: body
+	data struct {
+		Code    string `json:"code"`
+		Message string `json:"error"`
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//////////////////                  REQUESTS                /////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+// swagger:parameters  deleteID getID
+type buyerRequestID struct {
+	// Value corresponding to object ID on db.
+	// in: path
+	// required: true
+	Id string
+}
+
+// swagger:parameters createBuyer
+type buyerNewResquest struct {
+	//in: body
+	NewBuyer models.Buyer
+}
+
+// swagger:parameters patchBuyer
+type patchBuyer struct {
+	// Corresponding object on db.
+	//in: path
+	Id string
+	// New values
+	//in: body
+	PatchValues updateBuyer
+}
+
+type updateBuyer struct {
+	CardNumberID string `json:"card_number_id" binding:"required"`
+	FirstName    string `json:"first_name" binding:"required"`
+	LastName     string `json:"last_name" binding:"required"`
+}
--- go-web/go.mod
+++ go-web/go.mod
@@ -8,14 +8,13 @@ require (
 	github.com/gin-gonic/gin v1.8.1
 	github.com/go-playground/validator/v10 v10.11.0
 	github.com/google/uuid v1.3.0
-	github.com/pdrum/swagger-automation v0.0.0-20190629163613-c8c7c80ba858
-	github.com/stretchr/testify v1.7.1
+	github.com/stretchr/testify v1.7.2
 	golang.org/x/exp v0.0.0-20220602145555-4a0574d9293f
 )
 
 require (
-	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/assert/v2 v2.0.1
 	github.com/go-playground/locales v0.14.0 // indirect
@@ -23,26 +22,21 @@ require (
 	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/labstack/echo v3.3.10+incompatible // indirect
-	github.com/labstack/gommon v0.2.9 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
-	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/rogpeppe/go-internal v1.8.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.1.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.1 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasttemplate v1.0.1 // indirect
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
 	golang.org/x/net v0.0.0-20220621193019-9d032be2e588 // indirect
 	golang.org/x/sys v0.0.0-20220622161953-175b2fd9d664 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
