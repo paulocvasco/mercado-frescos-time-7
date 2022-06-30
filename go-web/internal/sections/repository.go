bb601309b5ba1a1b238686874c070812bf477702
jobelmonte <joao.belmonte@mercadolivre.com.br>
2022-06-30 16:12:41 -0300

    change to metadata struct in repo

--- go-web/internal/sections/repository.go
+++ go-web/internal/sections/repository.go
@@ -8,27 +8,15 @@ import (
 
 type Section models.Section
 
-var storage models.Sections
-
 type repository struct {
 	database db.DB
 }
 
-func (s *repository)ValidateID(id int) bool {
-	err := s.database.Load(&storage)
-	if err != nil {
-		return false
-	}
-
-	if id < 0 || id > storage.LastID {
-		return false
-	}
-	return true
-}
+var cache models.SectionMetaData
 
 //go:generate mockery --name=Repository --output=./mock/mockRepository --outpkg=mockRepository
 type Repository interface {
-	GetAll() ([]models.Section, error)
+	GetAll() (models.Sections, error)
 	GetById(int) (models.Section, error)
 	Store(Section) (Section, error)
 	Update(int, Section) error
@@ -43,20 +31,21 @@ func NewRepository(database db.DB) Repository {
 	}
 }
 
-func (s *repository) GetAll() ([]models.Section, error) {
-	err := s.database.Load(&storage)
+func (s *repository) GetAll() (models.Sections, error) {
+
+	err := s.database.Load(&cache)
 	if err != nil {
-		return []models.Section{}, nil
+		return models.Sections{}, nil
 	}
-	return storage.SectionList, nil
+	return cache.Content, nil
 }
 
 func (s *repository) GetById(id int) (models.Section, error) {
-	err := s.database.Load(&storage)
+	err := s.database.Load(&cache)
 	if err != nil {
 		return models.Section{}, err
 	}
-	for _, section := range storage.SectionList {
+	for _, section := range cache.Content.SectionList {
 		if section.ID == id {
 			return section, nil
 		}
@@ -65,7 +54,7 @@ func (s *repository) GetById(id int) (models.Section, error) {
 }
 
 func (s *repository) VerifySectionNumber(sectionRequestedNumber int) error {
-	for _, section := range storage.SectionList {
+	for _, section := range cache.Content.SectionList {
 		if section.SectionNumber == sectionRequestedNumber {
 			return customErrors.ErrorConflict
 		}
@@ -74,14 +63,15 @@ func (s *repository) VerifySectionNumber(sectionRequestedNumber int) error {
 }
 
 func (s *repository) Store(newSection Section) (Section, error) {
-	err := s.database.Load(&storage)
+	var sections models.SectionMetaData
+	err := s.database.Load(&sections)
 	if err != nil {
 		return Section{}, err
 	}
-	newSection.ID = storage.LastID
-	storage.SectionList = append(storage.SectionList, models.Section(newSection))
-	storage.LastID = storage.LastID + 1
-	err = s.database.Save(&storage)
+	newSection.ID = sections.LastID
+	sections.Content.SectionList = append(sections.Content.SectionList, models.Section(newSection))
+	sections.LastID = sections.LastID + 1
+	err = s.database.Save(&sections)
 
 	if err != nil {
 		return Section{}, err
@@ -91,13 +81,13 @@ func (s *repository) Store(newSection Section) (Section, error) {
 }
 
 func (s *repository) Update(id int, newSection Section) error {
-	for i, section := range storage.SectionList {
+	for i, section := range cache.Content.SectionList {
 		if section.ID == newSection.ID {
 
 			section = models.Section(newSection)
-			storage.SectionList[i] = section
+			cache.Content.SectionList[i] = section
 
-			err := s.database.Save(&storage)
+			err := s.database.Save(&cache)
 			if err != nil {
 				return err
 			}
@@ -108,15 +98,15 @@ func (s *repository) Update(id int, newSection Section) error {
 }
 
 func (s *repository) Delete(id int) error {
-	err := s.database.Load(&storage)
+	err := s.database.Load(&cache)
 	if err != nil {
 		return err
 	}
 
-	for i, section := range storage.SectionList {
+	for i, section := range cache.Content.SectionList {
 		if section.ID == id {
-			storage.SectionList = append(storage.SectionList[:i], storage.SectionList[i+1:]...)
-			err = s.database.Save(&storage)
+			cache.Content.SectionList = append(cache.Content.SectionList[:i], cache.Content.SectionList[i+1:]...)
+			err = s.database.Save(&cache)
 			if err != nil {
 				return err
 			}
@@ -124,4 +114,16 @@ func (s *repository) Delete(id int) error {
 		}
 	}
 	return customErrors.ErrorInvalidID
+}
+
+func (s *repository) ValidateID(id int) bool {
+	err := s.database.Load(&cache)
+	if err != nil {
+		return false
+	}
+
+	if id < 0 || id > cache.LastID {
+		return false
+	}
+	return true
 }
