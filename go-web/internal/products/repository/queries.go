3dc8d4d5d17f33c4bef5c7c512d5a71a438fa8ea
rodneyems <rodney.martins@live.com>
2022-07-05 16:12:11 -0300

    refactor: fix arguments

--- go-web/internal/products/repository/queries.go
+++ go-web/internal/products/repository/queries.go
@@ -5,7 +5,7 @@ const (
 
 	sqlGetById = "SELECT * FROM products WHERE id = ?"
 
-	sqlStore = "INSERT INTO products (description, expiration_rate, freezing_rate, height, length, net_weight, product_code, recommended_freezing_temperature, width, product_type_id, seller_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+	sqlStore = "INSERT INTO products (description, expiration_rate, freezing_rate, height, length, net_weight, product_code, recommended_freezing_temperature, width, product_type_id, seller_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
 	sqlUpdate = "UPDATE products SET description=?, expiration_rate=?, freezing_rate=?, height=?, length=?, net_weight=?, product_code=?, recommended_freezing_temperature=?, width=?, product_type_id=?, seller_id=? WHERE id=?"
 
