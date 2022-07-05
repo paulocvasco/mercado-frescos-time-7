2c57fc091646e5ef2793e848e948740b7b7adeef
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-07-05 15:01:24 -0300

    Implement logic to create

--- go-web/internal/warehouse/repository/mysql.go
+++ go-web/internal/warehouse/repository/mysql.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"fmt"
 	"mercado-frescos-time-7/go-web/internal/models"
 	"mercado-frescos-time-7/go-web/pkg/db/mysql"
 )
@@ -17,7 +18,18 @@ func NewSqlRepository() Repository {
 }
 
 func (m *mysqlDB) Create(new models.Warehouse) (models.Warehouse, error) {
-	return models.Warehouse{}, nil
+	arg := fmt.Sprintf("INSERT INTO warehouse(address, telephone, warehouse_code, minimum_capacity, minimum_temperature) VALUES ('%s', '%s', '%s', %d, %d)", new.Address, new.Telephone, new.WarehouseCode, new.MinimunCapacity, new.MinimunTemperature)
+	res, err := m.db.Exec(arg)
+	if err != nil {
+		return models.Warehouse{}, err
+	}
+	lastID, err := res.LastInsertId()
+	if err != nil {
+		return models.Warehouse{}, err
+	}
+	newWarehouse := new
+	newWarehouse.ID = int(lastID)
+	return newWarehouse, nil
 }
 
 func (m *mysqlDB) Update(id int, patchedWarehouse models.Warehouse) error {
