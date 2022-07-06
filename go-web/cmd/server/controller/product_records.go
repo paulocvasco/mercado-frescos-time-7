b2e92e9bad5946c71481e04b9a0ca4b2504ffd62
rodneyems <rodney.martins@live.com>
2022-07-06 12:28:08 -0300

    feature: add product record controller

---
+++ go-web/cmd/server/controller/product_records.go
@@ -0,0 +1,94 @@
+package controller
+
+import (
+	"encoding/json"
+	productrecords "mercado-frescos-time-7/go-web/internal/product_records"
+	customerrors "mercado-frescos-time-7/go-web/pkg/custom_errors"
+	"mercado-frescos-time-7/go-web/pkg/web"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type ProductRecordsController interface {
+	GetProductRecordsById() gin.HandlerFunc
+	InsertProductRecord() gin.HandlerFunc
+}
+
+type productRecordsController struct {
+	service productrecords.Service
+}
+
+func NewProductRecordsController(service productrecords.Service) ProductRecordsController {
+	return &productRecordsController{
+		service: service,
+	}
+}
+
+func (prc *productRecordsController) GetProductRecordsById() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Query("id"))
+		if err != nil {
+			status, msg := customerrors.ErrorHandleResponse(err)
+			res := web.NewResponse(status, nil, msg)
+			c.JSON(status, res)
+			return
+		}
+		records, err := prc.service.GetByProductId(id)
+		if err != nil {
+			status, msg := customerrors.ErrorHandleResponse(err)
+			res := web.NewResponse(status, nil, msg)
+			c.JSON(status, res)
+			return
+		}
+		c.JSON(http.StatusOK, web.NewResponse(http.StatusOK, records, ""))
+	}
+}
+
+func (prc *productRecordsController) InsertProductRecord() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		newRecord := productRecordInsert{}
+		err := c.ShouldBind(&newRecord)
+		if err != nil {
+			status, msg := customerrors.ErrorHandleResponse(err)
+			res := web.NewResponse(status, nil, msg)
+			c.JSON(status, res)
+			return
+		}
+
+		date, err := time.Parse("2006-01-02", newRecord.LastUpdateDate)
+		if err != nil {
+			status, msg := customerrors.ErrorHandleResponse(err)
+			res := web.NewResponse(status, nil, msg)
+			c.JSON(status, res)
+			return
+		}
+
+		newRecord.LastUpdateDate = date.Format(time.RFC3339)
+
+		prJSON, err := json.Marshal(newRecord)
+		if err != nil {
+			status, msg := customerrors.ErrorHandleResponse(err)
+			res := web.NewResponse(status, nil, msg)
+			c.JSON(status, res)
+			return
+		}
+		pr, err := prc.service.Insert(prJSON)
+		if err != nil {
+			status, msg := customerrors.ErrorHandleResponse(err)
+			res := web.NewResponse(status, nil, msg)
+			c.JSON(status, res)
+			return
+		}
+		c.JSON(http.StatusCreated, web.NewResponse(http.StatusCreated, pr, ""))
+	}
+}
+
+type productRecordInsert struct {
+	LastUpdateDate string  `json:"last_update_date" binding:"required"`
+	PurchasePrince float64 `json:"purchase_prince" binding:"required"`
+	SalePrice      float64 `json:"sale_price" binding:"required"`
+	ProductId      int     `json:"product_id" binding:"required"`
+}
