381456db1f9ea141c95bcccbd7afec6d264c9a19
rodneyems <rodney.martins@live.com>
2022-07-07 11:45:06 -0300

    feature: add query to find all

--- go-web/internal/product_records/repository/queries.go
+++ go-web/internal/product_records/repository/queries.go
@@ -3,5 +3,7 @@ package repository
 const (
 	sqlStoreRecord = "INSERT INTO product_records (last_update_date, purchase_prince, sale_price, product_id) VALUES (?, ?, ?, ?)"
 	
-	sqlGetRecordById = "SELECT * FROM product_records WHERE product_id = ?"
+	sqlGetRecordById = "SELECT pr.product_id, p.description, COUNT(*) AS records_count FROM product_records pr INNER JOIN products p ON pr.product_id = p.id WHERE pr.product_id = ? GROUP BY pr.product_id"
+
+	sqlGetAllRecords = "SELECT pr.product_id, p.description, COUNT(*) AS records_count FROM product_records pr INNER JOIN products p ON pr.product_id = p.id WHERE pr.product_id > ? GROUP BY pr.product_id"
 )
