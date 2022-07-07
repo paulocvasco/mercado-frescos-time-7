6936b1923a7817f04307e82860f655504d82aa53
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-07-07 19:28:24 -0300

    Implement carrier repository methods

--- go-web/internal/carriers/repository/mysql.go
+++ go-web/internal/carriers/repository/mysql.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"database/sql"
 	"mercado-frescos-time-7/go-web/internal/models"
+	customerrors "mercado-frescos-time-7/go-web/pkg/custom_errors"
 	"mercado-frescos-time-7/go-web/pkg/db"
 )
 
@@ -23,9 +24,81 @@ func NewRepository() Repository {
 }
 
 func (m *mysqlDB) Create(new models.Carrier) (models.Carrier, error) {
-	return models.Carrier{}, nil
+	query := "INSERT INTO carriers(cid, company_name, address, locality_id) VALUES (?, ?, ?, ?)"
+	stmt, err := m.db.Prepare(query)
+	if err != nil {
+		return models.Carrier{}, err
+	}
+	res, err := stmt.Exec(new.Cid, new.Company, new.Address, new.LocalityID)
+	if err != nil {
+		return models.Carrier{}, err
+	}
+	ra, err := res.RowsAffected()
+	if err != nil {
+		return models.Carrier{}, err
+	}
+	if ra == 0 {
+		return models.Carrier{}, customerrors.ErrorStoreFailed
+	}
+
+	lastID, err := res.LastInsertId()
+	if err != nil {
+		return models.Carrier{}, err
+	}
+
+	new.ID = int(lastID)
+	return new, nil
 }
 
 func (m *mysqlDB) Get(id int) (models.CarriersReport, error) {
-	return models.CarriersReport{}, nil
+	var report models.CarriersReport
+	var err error
+	if id == 0 {
+		report, err = m.getAll()
+	} else {
+		report, err = m.getById(id)
+	}
+	return report, err
+}
+
+func (m *mysqlDB) getById(id int) (models.CarriersReport, error) {
+	query := "SELECT * FROM carriers WHERE locality_id = ?"
+	res, err := m.db.Query(query, id)
+	if err != nil {
+		return models.CarriersReport{}, err
+	}
+
+	var report models.CarriersReport
+	for res.Next() {
+		var r models.CarrierInfo
+		err := res.Scan(&r.LocalityID, &r.LocalityName, &r.CarriersCount)
+		if err != nil {
+			return models.CarriersReport{}, err
+		}
+		report.Data = append(report.Data, r)
+	}
+	return report, nil
+}
+
+func (m *mysqlDB) getAll() (models.CarriersReport, error) {
+	query := `SELECT c.locality_id, l.locality_name, COUNT(*) AS carriers_count FROM carrier c
+INNER JOIN locality l
+ON c.locality_id = l.id
+GROUP BY c.locality_id;`
+
+	res, err := m.db.Query(query)
+	if err != nil {
+		return models.CarriersReport{}, err
+	}
+
+	var report models.CarriersReport
+	for res.Next() {
+		var r models.CarrierInfo
+		err := res.Scan(&r.LocalityID, &r.LocalityName, &r.CarriersCount)
+		if err != nil {
+			return models.CarriersReport{}, err
+		}
+		report.Data = append(report.Data, r)
+	}
+	return report, nil
 }
