5365a3625089bcfb0c1ce0dc297c6f0d0af84c2a
Paulo Cesar Vasco De Souza <paulo.vasco@mercadolivre.com>
2022-07-08 14:13:01 -0300

    Change logic to avoid duplicate code

--- go-web/internal/carriers/repository/mysql.go
+++ go-web/internal/carriers/repository/mysql.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"fmt"
 	"mercado-frescos-time-7/go-web/internal/models"
 	customerrors "mercado-frescos-time-7/go-web/pkg/custom_errors"
 	"mercado-frescos-time-7/go-web/pkg/db"
@@ -51,37 +52,12 @@ func (m *mysqlDB) Create(new models.Carrier) (models.Carrier, error) {
 }
 
 func (m *mysqlDB) Get(id int) (models.CarriersReport, error) {
-	var report models.CarriersReport
-	var err error
+	var query string
 	if id == 0 {
-		report, err = m.getAll()
+		query = "SELECT c.locality_id, l.locality_name, COUNT(*) AS carriers_count FROM carriers c INNER JOIN localities l ON c.locality_id = l.id GROUP BY c.locality_id"
 	} else {
-		report, err = m.getById(id)
+		query = fmt.Sprintf("SELECT c.locality_id, l.locality_name, COUNT(*) AS carriers_count FROM carriers c INNER JOIN localities l ON c.locality_id = l.id WHERE c.locality_id = %d GROUP BY c.locality_id", id)
 	}
-	return report, err
-}
-
-func (m *mysqlDB) getById(id int) (models.CarriersReport, error) {
-	query := "SELECT c.locality_id, l.locality_name, COUNT(*) AS carriers_count FROM carriers c INNER JOIN localities l ON c.locality_id = l.id WHERE c.locality_id = ? GROUP BY c.locality_id"
-	res, err := m.db.Query(query, id)
-	if err != nil {
-		return models.CarriersReport{}, err
-	}
-
-	var report models.CarriersReport
-	for res.Next() {
-		var r models.CarrierInfo
-		err := res.Scan(&r.LocalityID, &r.LocalityName, &r.CarriersCount)
-		if err != nil {
-			return models.CarriersReport{}, err
-		}
-		report.Data = append(report.Data, r)
-	}
-	return report, nil
-}
-
-func (m *mysqlDB) getAll() (models.CarriersReport, error) {
-	query := "SELECT c.locality_id, l.locality_name, COUNT(*) AS carriers_count FROM carriers c INNER JOIN localities l ON c.locality_id = l.id GROUP BY c.locality_id"
 
 	res, err := m.db.Query(query)
 	if err != nil {
