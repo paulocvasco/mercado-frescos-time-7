6becf0d7604bf0cf9b6659ce1753586794891727
jandrade13 <jefferson.jandrade@mercadolivre.com>
2022-07-11 10:08:51 -0300

    created documentation

---
+++ go-web/docs/purchaseOrders.go
@@ -0,0 +1,62 @@
+package docs
+
+import (
+	"mercado-frescos-time-7/go-web/internal/models"
+)
+
+/////////////////////////////////////////////////////////////////////////////////////
+//////////////////               END POINTS               ///////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+// swagger:route POST /PurchaseOrder/ PurchaseOrder createPurchaseOrder
+// Add a new object on db.
+// responses:
+//    201: PurchaseOrderIDResponse
+//    400: errorResponse
+//    409: errorResponse
+//    422: errorResponse
+//    500: errorServerResponse
+
+/////////////////////////////////////////////////////////////////////////////////////
+//////////////////                  RESPONSES               /////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+// Corresponding object from db on json format.
+// swagger:response PurchaseOrderIDResponse
+type PurchaseOrderIDResponse struct {
+	//in: body
+	data struct {
+		Code string       `json:"code"`
+		Data models.Buyer `json:"data"`
+	}
+}
+
+// Error message has the returned code and a descripton to help understand the cause.
+// swagger:response errorResponse
+type errorResponsePurchaseOrder struct {
+	// in: body
+	data struct {
+		Code    string `json:"code"`
+		Message string `json:"error"`
+	}
+}
+
+// Error has the returned when exists a server error.
+// swagger:response errorServerResponse
+type errorServerResponsePurchaseOrder struct {
+	// in: body
+	data struct {
+		Code    string `json:"code"`
+		Message string `json:"error"`
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//////////////////                  REQUESTS                /////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+// swagger:parameters  createPurchaseOrder
+type PurchaseOrderNewResquest struct {
+	//in: body
+	NewPurchaseOrder models.PurchaseOrders
+}
