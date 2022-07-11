33baa567b55244e86548414ae72aa9459c52cf42
jandrade13 <jefferson.jandrade@mercadolivre.com>
2022-07-11 16:17:50 -0300

    excluded line

--- go-web/pkg/db/mysql.go
+++ go-web/pkg/db/mysql.go
@@ -5,26 +5,19 @@ import (
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
-
-	_ "github.com/go-sql-driver/mysql"
 )
 
 var StorageDB *sql.DB
 
 func init() {
-	// user := os.Getenv("USERDB")
-	// password := os.Getenv("PASSWORDDB")
-	// portDB := os.Getenv("PORT")
-	// nameDB := os.Getenv("NAMEDB")
-	// dataSource := fmt.Sprint("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, portDB, nameDB)
+	// path, _ := os.Getwd()
+	// err := godotenv.Load(filepath.Join(path, ".env"))
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
+	// dataSource := fmt.Sprintf("%v:%v@tcp(%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("USER_DB"), os.Getenv("PASS_DB"), os.Getenv("PATH_DB"), os.Getenv("NAME_DB"))
 	dataSource := "root:12345678@tcp(localhost:3306)/mercado_fresco_db?parseTime=true"
 	var err error
-	// err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	// dataSource := fmt.Sprintf("%v:%v@tcp(%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("USER_DB"), os.Getenv("PASS_DB"), os.Getenv("PATH_DB"), os.Getenv("NAME_DB"))
 	StorageDB, err = sql.Open("mysql", dataSource)
 	if err != nil {
 		panic(err)
