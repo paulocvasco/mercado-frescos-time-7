140ec89a90a3594a3af1a167876909d417abab22
jobelmonte <joao.belmonte@mercadolivre.com.br>
2022-07-11 11:06:28 -0300

    fixing db instance in product batch routes

--- go-web/cmd/server/routes/product_batch.go
+++ go-web/cmd/server/routes/product_batch.go
@@ -1,16 +1,15 @@
 package routes
 
 import (
-	"database/sql"
 	"github.com/gin-gonic/gin"
 	"mercado-frescos-time-7/go-web/cmd/server/controller"
 	"mercado-frescos-time-7/go-web/internal/productBatch"
 	"mercado-frescos-time-7/go-web/internal/productBatch/repository"
+	"mercado-frescos-time-7/go-web/pkg/db"
 )
 
 func InstanceProductBatch(eng *gin.Engine) {
-	var database *sql.DB
-	repository := repository.NewRepository(database)
+	repository := repository.NewRepositoryProductBatch(db.StorageDB)
 	services := productBatch.NewService(repository)
 	controller := controller.NewControllerProductBatch(services)
 	pb := eng.Group("/api/v1/productBatches")
