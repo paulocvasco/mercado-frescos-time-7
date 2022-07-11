df75ee55279880dc25d19aa13cae26e0b3366f1c
rodneyems <rodney.martins@live.com>
2022-07-11 16:42:30 -0300

    fix: col name

--- go-web/internal/product_records/repository/queries.go
+++ go-web/internal/product_records/repository/queries.go
@@ -1,7 +1,7 @@
 package repository
 
 const (
-	sqlStoreRecord = "INSERT INTO product_records (last_update_date, purchase_prince, sale_price, product_id) VALUES (?, ?, ?, ?)"
+	sqlStoreRecord = "INSERT INTO product_records (last_update_date, purchase_price, sale_price, product_id) VALUES (?, ?, ?, ?)"
 	
 	sqlGetRecordById = "SELECT pr.product_id, p.description, COUNT(*) AS records_count FROM product_records pr INNER JOIN products p ON pr.product_id = p.id WHERE pr.product_id = ? GROUP BY pr.product_id"
 
