727175f99539bccc57cd3cf7ca19b517aa5f9bc4
Sebastião Fortes <sebastiaofortes4@gmail.com>
2022-07-12 18:29:23 -0300

    corrigindo testes

--- go-web/internal/locality/repository/mysql_test.go
+++ go-web/internal/locality/repository/mysql_test.go
@@ -14,7 +14,7 @@ func TestStoreLocality(t *testing.T) {
 	u := models.Locality{Id:"1",Locality_name: "Buritizeiro",Province_name: "Minas Gerais", Country_name: "Brazil"}
 	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
 	assert.NoError(t, err)
-	mock.ExpectQuery("SELECT a.id FROM provincies a INNER JOIN countries b ON a.id_country_fk = b.Id AND b.country_name = ? AND a.provincie_name = ?;").
+	mock.ExpectQuery("SELECT a.id FROM provinces a INNER JOIN countries b ON a.id_country_fk = b.Id AND b.country_name = ? AND a.province_name = ?;").
 	WithArgs(u.Country_name, u.Province_name).WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
 
 	mock.ExpectPrepare("INSERT INTO `localities` (`id`, `locality_name`, `province_id`) VALUES (?, ?, ?)").ExpectExec().WithArgs(u.Id, u.Locality_name, 1).WillReturnResult(sqlmock.NewResult(1, 1))
@@ -30,7 +30,7 @@ func TestStoreLocalityErrorPrepare(t *testing.T) {
 	u := models.Locality{Id:"1",Locality_name: "Buritizeiro",Province_name: "Minas Gerais", Country_name: "Brazil"}
 	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
 	assert.NoError(t, err)
-	mock.ExpectQuery("SELECT a.id FROM provincies a INNER JOIN countries b ON a.id_country_fk = b.Id AND b.country_name = ? AND a.provincie_name = ?;").
+	mock.ExpectQuery("SELECT a.id FROM provinces a INNER JOIN countries b ON a.id_country_fk = b.Id AND b.country_name = ? AND a.province_name = ?;").
 	WithArgs(u.Country_name, u.Province_name).WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
 
 	mock.ExpectPrepare("INSERT INTO `localities` (`id`, `locality_name`, `province_id`) VALUES (?, ?, ?)").
@@ -47,7 +47,7 @@ func TestStoreLocalityErrorExec(t *testing.T) {
 	u := models.Locality{Id:"1",Locality_name: "Buritizeiro",Province_name: "Minas Gerais", Country_name: "Brazil"}
 	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
 	assert.NoError(t, err)
-	mock.ExpectQuery("SELECT a.id FROM provincies a INNER JOIN countries b ON a.id_country_fk = b.Id AND b.country_name = ? AND a.provincie_name = ?;").
+	mock.ExpectQuery("SELECT a.id FROM provinces a INNER JOIN countries b ON a.id_country_fk = b.Id AND b.country_name = ? AND a.province_name = ?;").
 	WithArgs(u.Country_name, u.Province_name).WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
 
 	mock.ExpectPrepare("INSERT INTO `localities` (`id`, `locality_name`, `province_id`) VALUES (?, ?, ?)").ExpectExec().WillReturnError(customerrors.ErrorInvalidDB)
@@ -63,7 +63,7 @@ func TestStoreLocalityErrorQuery(t *testing.T) {
 	u := models.Locality{Id:"1",Locality_name: "Buritizeiro",Province_name: "Minas Gerais", Country_name: "Brazil"}
 	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
 	assert.NoError(t, err)
-	mock.ExpectQuery("SELECT a.id FROM provincies a INNER JOIN countries b ON a.id_country_fk = b.Id AND b.country_name = ? AND a.provincie_name = ?;").WillReturnError(customerrors.ErrorConflict)
+	mock.ExpectQuery("SELECT a.id FROM provinces a INNER JOIN countries b ON a.id_country_fk = b.Id AND b.country_name = ? AND a.province_name = ?;").WillReturnError(customerrors.ErrorConflict)
 
 	repo := repository.NewSQLrepository(db)
 
